4f9cec5aad55dddeb930ce4b04f58ea4ac463d87
Shen, Lin <linx.a.shen@intel.com>
2014-03-19 09:35:21 +0800

    |wrt-misc| delete chmod and su app command
    Signed-off-by: Shen, Lin <linx.a.shen@intel.com>

--- wrt/tct-stab-wrt-tests/scripts/xwalk_common.sh
+++ wrt/tct-stab-wrt-tests/scripts/xwalk_common.sh
@@ -32,22 +32,22 @@
 
 ##usage: install_app $app_path(e.g. uninstall_app /opt/usr/media/tct/opt/tct-sp02-wrt-tests/tct-sp02-wrt-tests.wgt)##
 function install_app(){
-    su - app -c "export DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/5000/dbus/user_bus_socket\";export XDG_RUNTIME_DIR=\"/run/user/5000\";xwalkctl --install $1"
+    xwalkctl --install $1
 }
 
 ##usage: uninstall_app $app_name(e.g. uninstall_app tct-sp02-wrt-tests)##
 function uninstall_app(){
-    su - app -c "export DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/5000/dbus/user_bus_socket\";export XDG_RUNTIME_DIR=\"/run/user/5000\";xwalkctl" >/tmp/apps.txt 2>&1
+    xwalkctl >/tmp/apps.txt 2>&1
     pkgids=`cat /tmp/apps.txt | grep $1 | awk '{print $(NF-1)}'`
     for pkgid in $pkgids
     do
-        su - app -c "export DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/5000/dbus/user_bus_socket\";export XDG_RUNTIME_DIR=\"/run/user/5000\";xwalkctl --uninstall $pkgid"
+        xwalkctl --uninstall $pkgid
     done
 }
 
 ##usage: find_app $app_name(e.g. uninstall_app tct-sp02-wrt-tests)##
 function find_app(){
-    su - app -c "export DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/5000/dbus/user_bus_socket\";export XDG_RUNTIME_DIR=\"/run/user/5000\";xwalkctl" >/tmp/apps.txt 2>&1
+    xwalkctl >/tmp/apps.txt 2>&1
     pkgids=`cat /tmp/apps.txt | grep $1 | awk '{print $(NF-1)}'`
 }
 
@@ -56,7 +56,7 @@ function launch_app(){
     find_app $1
     pkgnum=`echo "$pkgids"|wc -w`
     if [ $pkgnum -eq 1 ]; then
-        su - app -c "export DBUS_SESSION_BUS_ADDRESS=\"unix:path=/run/user/5000/dbus/user_bus_socket\";export XDG_RUNTIME_DIR=\"/run/user/5000\";nohup xwalk-launcher $pkgids &>/dev/null &"
+        nohup xwalk-launcher $pkgids &>/dev/null &
     else
         echo "launch error, please check if exists this app or there are more than one app with this app_name"
     fi
