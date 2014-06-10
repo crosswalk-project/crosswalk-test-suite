6b7e11703ae4c6d784a6676770d9b5d141a2c777
Li Hao <haox.li@intel.com>
2014-06-10 17:15:18 +0800

    [webapi] Improve exception message for zap security
    - Use "error" instead of exception message

--- webapi/tct-xmlhttprequest-w3c-tests/xmlhttprequest-py/w3c/resources/form.py
+++ webapi/tct-xmlhttprequest-w3c-tests/xmlhttprequest-py/w3c/resources/form.py
@@ -2,4 +2,4 @@ def main(request, response):
     try:
         return "id:%s;value:%s;" % (request.POST.first("id"), request.POST.first("value"))
     except Exception,ex:
-        return Exception,":",ex
+        return "error"
--- webapi/webapi-resourcetiming-w3c-tests/resourcetiming-py/w3c/submission/resources/generate_resource.py
+++ webapi/webapi-resourcetiming-w3c-tests/resourcetiming-py/w3c/submission/resources/generate_resource.py
@@ -2,56 +2,59 @@ import os
 
 
 def main(request, response):
-  get_type = request.GET.first("types")
-
-  status = 200
-  body = ""
-  content_type = "text/plain"
-
-  def file_get_contents(path):
-    file_object = open(os.path.split(os.path.realpath(__file__))[0] + os.path.sep + path)
     try:
-      content = file_object.read()
-    finally:
-      file_object.close()
-    return content
+        get_type = request.GET.first("types")
 
-  def audio():
-    body = file_get_contents("silence.wav")
-    content_type = "audio/wav"
+        status = 200
+        body = ""
+        content_type = "text/plain"
 
-  def css():
-    body = file_get_contents("gray_bg.css")
-    content_type = "text/css"
+        def file_get_contents(path):
+          file_object = open(os.path.split(os.path.realpath(__file__))[0] + os.path.sep + path)
+          try:
+            content = file_object.read()
+          finally:
+            file_object.close()
+          return content
 
-  def font():
-    body = file_get_contents("Ahem.ttf")
-    content_type = "application/octet-stream"
+        def audio():
+          body = file_get_contents("silence.wav")
+          content_type = "audio/wav"
 
-  def iframe():
-    body = file_get_contents("blank_page_green.htm")
-    content_type = "text/html"
+        def css():
+          body = file_get_contents("gray_bg.css")
+          content_type = "text/css"
 
-  def image():
-    body = file_get_contents("1x1-blue.png")
-    content_type = "image/png"
+        def font():
+          body = file_get_contents("Ahem.ttf")
+          content_type = "application/octet-stream"
 
-  def script():
-    body = file_get_contents("empty_script.js")
-    content_type = "text/javascript"
+        def iframe():
+          body = file_get_contents("blank_page_green.htm")
+          content_type = "text/html"
 
-  operator = {"audio": audio, "css": css,"font": font,"iframe": iframe,"image": image,"script": script}
+        def image():
+          body = file_get_contents("1x1-blue.png")
+          content_type = "image/png"
 
-  if operator.has_key(get_type):
-    operator.get(get_type)()
-  else:
-    status = 404
+        def script():
+          body = file_get_contents("empty_script.js")
+          content_type = "text/javascript"
 
-  headers = [("Content-Type", content_type)]
+        operator = {"audio": audio, "css": css,"font": font,"iframe": iframe,"image": image,"script": script}
 
-  if "cacheable" in request.headers:
-    headers.append(("Etag", 7))
-  else:
-    headers.append(("Cache-control", "no-cache"))
+        if operator.has_key(get_type):
+          operator.get(get_type)()
+        else:
+          status = 404
 
-  return status, headers, body
+        headers = [("Content-Type", content_type)]
+
+        if "cacheable" in request.headers:
+          headers.append(("Etag", 7))
+        else:
+          headers.append(("Cache-control", "no-cache"))
+
+        return status, headers, body
+    except Exception,ex:
+        return "error"
