af76209ed0e6b3d49cd1e561d594953d5950c3db
linx.a.shen <linx.a.shen@intel.com>
2014-07-07 11:09:41 +0800

    [wrt][XWALK890]update test.py 1. add negative seed out file 2. add log.txt file 3. add Chinese value in seed file
    UT result: total: 161, PASS 161

--- wrt/wrt-manifest-tizen-tests/const.py
+++ wrt/wrt-manifest-tizen-tests/const.py
@@ -15,6 +15,7 @@ else:
    seed_file_na = path + "/allpairs/input_seed_negative.txt"
    selfcomb_file = path + "/allpairs/selfcomb.txt"
    output_file = path + "/allpairs/output.txt"
+   output_file_ne = path + "/allpairs/output_negative.txt"
    report_path = path + "/report"
    report_file = report_path + "/wrt-manifest-tizen-tests.xml"
    report_summary_file = report_path + "/summary.xml"
