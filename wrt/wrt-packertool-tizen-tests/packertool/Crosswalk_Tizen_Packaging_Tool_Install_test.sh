19be7561f0a60caf21455601ab28bf61940da7ed
linx.a.shen <linx.a.shen@intel.com>
2014-09-19 10:46:44 +0800

    [wrt]update const.py path for wrt-manifest-tizen-tests
    [Detailed commit message (if have)]
    Impacted TCs num(approved): New 0, update 4, deleted 0
    Unit test platform: [tizen]
    Unit test result summary:Pass 0, Fail 2,Block 1
    Test create folder ok,block/fail by:
    https://crosswalk-project.org/jira/browse/XWALK-2290

--- wrt/wrt-packertool-tizen-tests/packertool/Crosswalk_Tizen_Packaging_Tool_Install_test.sh
+++ wrt/wrt-packertool-tizen-tests/packertool/Crosswalk_Tizen_Packaging_Tool_Install_test.sh
@@ -32,7 +32,8 @@
 #
 source $(dirname $0)/Common.sh
 
-python make_xpk.py diffid_same_version_tests/ key.pem 
+local_path=$(dirname $0)
+python $local_path/../make_xpk.py $local_path/../diffid_same_version_tests/ key.pem 
 sleep 5
 test -f diffid_same_version_tests.xpk
 if [ $? -eq 0 ];then
