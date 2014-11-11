e5d934cac0e7207b934d3f7e878539e9b0077952
Xie Yunxiao <yunxiaox.xie@intel.com>
2014-11-11 13:50:14 +0800

    [webapi] Integrate 259 tests for IndexedDB
     - designed: 1, approved: 258
     - about 9 tests have been deleted
    
    Impacted tests(approved): new 256, update 2, delete 0
    Unit test platform: [Tizen IVI][Android]
    Unit test result summary: pass 258, fail 0, block 0

--- webapi/tct-indexeddb-w3c-tests/indexeddb/w3c/support.js
+++ webapi/tct-indexeddb-w3c-tests/indexeddb/w3c/support.js
@@ -25,11 +25,11 @@ if (!window.indexedDB)
 }
 
 function assert_open_request_error(event) {
-    assert_unreached("Open request error: " + event.target.error.name); 
+    assert_unreached("Open request error: " + event.target.error.name);
 }
 
 function assert_deleteDatabase_request_error(event) {
-    assert_unreached("Delete database request error: " + event.target.error.name); 
+    assert_unreached("Delete database request error: " + event.target.error.name);
 }
 
 function assert_database_error(event) {
@@ -54,4 +54,85 @@ function assert_cursor_exists(cursor) {
 
 function assert_unexpected_complete() {
     assert_unreached("Unexpected complete event fired.");
+}
+
+function fail(test, desc) {
+    return test.step_func(function(e) {
+        if (e && e.message && e.target.error)
+            assert_unreached(desc + " (" + e.target.error.name + ": " + e.message + ")");
+        else if (e && e.message)
+            assert_unreached(desc + " (" + e.message + ")");
+        else if (e && e.target.readyState === 'done' && e.target.error)
+            assert_unreached(desc + " (" + e.target.error.name + ")");
+        else
+            assert_unreached(desc);
+    });
+}
+
+function createdb(test, dbname, version)
+{
+    var rq_open = createdb_for_multiple_tests(dbname, version);
+    return rq_open.setTest(test);
+}
+
+function createdb_for_multiple_tests(dbname, version) {
+    var rq_open,
+        fake_open = {},
+        test = null,
+        dbname = (dbname ? dbname : "testdb-" + new Date().getTime() + Math.random() );
+
+    if (version)
+        rq_open = window.indexedDB.open(dbname, version);
+    else
+        rq_open = window.indexedDB.open(dbname);
+
+    function auto_fail(evt, current_test) {
+        /* Fail handlers, if we haven't set on/whatever/, don't
+         * expect to get event whatever. */
+        rq_open.manually_handled = {}
+
+        rq_open.addEventListener(evt, function(e) {
+            if (current_test !== test) {
+                return;
+            }
+
+            test.step(function() {
+                if (!rq_open.manually_handled[evt]) {
+                    assert_unreached("unexpected open." + evt + " event");
+                }
+
+                if (e.target.result + "" == "[object IDBDatabase]" && !this.db) {
+                    this.db = e.target.result;
+
+                    this.db.onerror = fail(test, "unexpected db.error");
+                    this.db.onabort = fail(test, "unexpected db.abort");
+                    this.db.onversionchange = fail(test, "unexpected db.versionchange");
+                }
+            })
+        })
+        rq_open.__defineSetter__("on" + evt, function(h) {
+            rq_open.manually_handled[evt] = true;
+            if (!h)
+                rq_open.addEventListener(evt, function() {});
+            else
+                rq_open.addEventListener(evt, test.step_func(h));
+        })
+    }
+
+    // add a .setTest method to the DB object
+    Object.defineProperty(rq_open, 'setTest', {
+        enumerable: false,
+        value: function(t) {
+            test = t;
+
+            auto_fail("upgradeneeded", test);
+            auto_fail("success", test);
+            auto_fail("blocked", test);
+            auto_fail("error", test);
+
+            return this;
+        }
+    });
+
+    return rq_open;
 }
