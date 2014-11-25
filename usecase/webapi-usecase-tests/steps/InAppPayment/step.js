c460fa94c9ead47bbccdad7417530b4e9b70f52b
Liu Yun <yunx.liu@intel.com>
2014-11-25 13:40:46 +0800

    [usecase-webapi][XWALK-893] Add a usecase for IAP
    - Add a usecase for IAP to query product details and buy gas product
    - Failure analysis: "In App Purchase" feature does not allowed in China
    
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: [Android]
    Unit test result summary: pass 0, fail 1, block 0

---
+++ usecase/webapi-usecase-tests/steps/InAppPayment/step.js
@@ -0,0 +1,6 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Purpose: </p>'
+            +'<p>Verifies query product details and buy gas product successfully.</p>'
+            +'<p>Expected Result: </p>'
+            +'<p>Test passes if query product details and buy gas product successfully.</p>'
+          +'</font>';
