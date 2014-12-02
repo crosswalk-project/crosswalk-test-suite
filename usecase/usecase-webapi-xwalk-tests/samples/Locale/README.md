378252f149273637a16810fb2b4912ca77e9296e
Xu Kang <kangx.xu@intel.com>
2014-12-02 17:51:27 +0000

    [usecase][TC-1813] Add 1 usecase for Locale API
    Impacted tests(approved) : new 1, update 0, delete 0
    Unit test platform: [IVI]
    Unit test result summary: pass 1, fail 0, block 0

---
+++ usecase/usecase-webapi-xwalk-tests/samples/Locale/README.md
@@ -0,0 +1,5 @@
+## Usecase Design
+
+This sample demonstrates Locale API feature basic functionalities, include:
+
+* Get locale information
---
+++ usecase/usecase-webapi-xwalk-tests/steps/Locale/step.js
@@ -0,0 +1,6 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Purpose: </p>'
+            +'<p>Verifies the Locale information can be got.</p>'
+            +'<p>Expected Result: </p>'
+            +'<p>Test passes if there is locale information displayed in page.</p>'
+          +'</font>';
