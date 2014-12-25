4a203f6f8781bfec83ff4c0daab840b7448af4de
linx.a.shen <linx.a.shen@intel.com>
2014-12-25 16:26:05 +0800

    [master][wrt][XWALK-3070] Add 2 new TCs
    Impacted TCs num(approved):New 2,Update 0,Delete 0
    Test Result: pass 2, fail 0, block 0

---
+++ wrt/wrt-commandline-tizen-tests/commandline/Crosswalk_Xwalk_Backend_PackageFileInvalid_Apps.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (C) 2010 Intel Corporation
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# Author:
+# 
+
+get_BackendApp_command=`/usr/lib/xwalk/xwalk_backend -i "" 2>&1` 
+if [[ "$get_BackendApp_command" =~ "Failed" || "$get_BackendApp_command" = "" ]];then
+        echo "when package file is invalid,the xwalk_backend process is not correctly"
+        exit 1
+else
+        echo "when package file is invalid,the xwalk_backend process correctly"
+        exit 0
+fi
+
