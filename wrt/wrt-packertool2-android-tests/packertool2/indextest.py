732d5793c108dff495316f850d0fac3467e0480e
whj <hongjuanx.wang@intel.com>
2014-12-25 14:24:56 +0800

    [wrt] Add 10 TCs and Update 9 TCs for wrt-packertool2-android-tests
    Impacted TCs num(approved): New 10, Update 9, Delete 0
    Unit test Platform: Android
    Android test result summary: Pass 19, Fail 0, Block 0

--- wrt/wrt-packertool2-android-tests/packertool2/indextest.py
+++ wrt/wrt-packertool2-android-tests/packertool2/indextest.py
@@ -8,47 +8,35 @@ class TestPackertoolsFunctions(unittest.TestCase):
      
     def test_index1(self):
         comm.setUp()
-        cmd = "python %smake_apk.py --package=org.hello.world --name=world --arch=%s --mode=%s --app-root=%s --app-local-path=%s" % \
+        cmd = "python %smake_apk.py --package=org.xwalk.example --name=example --arch=%s --mode=%s --app-root=%s --app-local-path=%s" % \
         (comm.Pck_Tools, comm.ARCH, comm.MODE, comm.APP_PATH, comm.INDEX_PATH[0])
         packstatus = commands.getstatusoutput(cmd)
-        if packstatus[0] == 0:
-            print "Generate APK ----------------> OK!"
-            result = commands.getstatusoutput("ls")
-            self.assertIn(comm.AppName, result[1])
-        else:
-            print "Generate APK ----------------> Error!"
-            result = commands.getstatusoutput("ls")
-            self.assertNotIn(comm.AppName, result[1])
+        self.assertEquals(0, packstatus[0])
+        print "Generate APK ----------------> OK!"
+        result = commands.getstatusoutput("ls")
+        self.assertIn(comm.AppName, result[1])
         os.remove(comm.AppName)
 
     def test_index2(self):
         comm.setUp()
-        cmd = "python %smake_apk.py --package=org.hello.world --name=world --arch=%s --mode=%s --app-root=%s --app-local-path=%s" % \
+        cmd = "python %smake_apk.py --package=org.xwalk.example --name=example --arch=%s --mode=%s --app-root=%s --app-local-path=%s" % \
         (comm.Pck_Tools, comm.ARCH, comm.MODE, comm.APP_PATH, comm.INDEX_PATH[1])
         packstatus = commands.getstatusoutput(cmd)
-        if packstatus[0] == 0:
-            print "Generate APK ----------------> OK!"
-            result = commands.getstatusoutput("ls")
-            self.assertIn(comm.AppName, result[1])
-        else:
-            print "Generate APK ----------------> Error!"
-            result = commands.getstatusoutput("ls")
-            self.assertNotIn(comm.AppName, result[1])
+        self.assertEquals(0, packstatus[0])
+        print "Generate APK ----------------> OK!"
+        result = commands.getstatusoutput("ls")
+        self.assertIn(comm.AppName, result[1])
         os.remove(comm.AppName)
 
     def test_index3(self):
         comm.setUp()
-        cmd = "python %smake_apk.py --package=org.hello.world --name=world --arch=%s --mode=%s --app-root=%s --app-local-path=%s" % \
+        cmd = "python %smake_apk.py --package=org.xwalk.example --name=example --arch=%s --mode=%s --app-root=%s --app-local-path=%s" % \
         (comm.Pck_Tools, comm.ARCH, comm.MODE, comm.APP_PATH, comm.INDEX_PATH[2])
         packstatus = commands.getstatusoutput(cmd)
-        if packstatus[0] == 0:
-            print "Generate APK ----------------> OK!"
-            result = commands.getstatusoutput("ls")
-            self.assertIn(comm.AppName, result[1])
-        else:
-            print "Generate APK ----------------> Error!"
-            result = commands.getstatusoutput("ls")
-            self.assertNotIn(comm.AppName, result[1])
+        self.assertEquals(0, packstatus[0])
+        print "Generate APK ----------------> OK!"
+        result = commands.getstatusoutput("ls")
+        self.assertIn(comm.AppName, result[1])
         os.remove(comm.AppName)
 
 if __name__ == '__main__':  
--- wrt/wrt-packertool2-android-tests/packertool2/manifesttest.py
+++ wrt/wrt-packertool2-android-tests/packertool2/manifesttest.py
@@ -7,54 +7,41 @@ import comm
 class TestPackertoolsFunctions(unittest.TestCase):
     def test_manifest1(self):
         comm.setUp()
-        os.chdir("resource")
-        cmd = "python %smake_apk.py --package=org.hello.world --arch=%s --mode=%s --manifest=%s" % \
+        print os.getcwd()
+        os.chdir(comm.ConstPath + "/../testapp/example")
+        cmd = "python %smake_apk.py --package=org.xwalk.example --arch=%s --mode=%s --manifest=%s" % \
         (comm.Pck_Tools, comm.ARCH, comm.MODE, comm.MANIFEST_PATH[0])
         packstatus = commands.getstatusoutput(cmd)
-        if packstatus[0] == 0:
-            print "Generate APK ----------------> OK!"
-            result = commands.getstatusoutput("ls")
-            self.assertIn(comm.AppName, result[1])
-        else:
-            print "Generate APK ----------------> Error!"
-            result = commands.getstatusoutput("ls")
-            self.assertNotIn(comm.AppName, result[1])
+        self.assertEquals(0, packstatus[0])
+        print "Generate APK ----------------> OK!"
+        result = commands.getstatusoutput("ls")
+        self.assertIn(comm.AppName, result[1])
         os.remove(comm.AppName)
-        os.chdir("..")
+        os.chdir("../..")
 
     def test_manifest2(self):
         comm.setUp()
-        os.chdir("resource")
-        cmd = "python %smake_apk.py --package=org.hello.world --arch=%s --mode=%s --manifest=%s" % \
+        os.chdir(comm.ConstPath + "/../testapp/example")
+        cmd = "python %smake_apk.py --package=org.xwalk.example --arch=%s --mode=%s --manifest=%s" % \
         (comm.Pck_Tools, comm.ARCH, comm.MODE, comm.MANIFEST_PATH[1])
         packstatus = commands.getstatusoutput(cmd)
-        print cmd
-        print packstatus
-        if packstatus[0] == 0:
-            print "Generate APK ----------------> OK!"
-            result = commands.getstatusoutput("ls")
-            self.assertIn(comm.AppName, result[1])
-        else:
-            print "Generate APK ----------------> Error!"
-            result = commands.getstatusoutput("ls")
-            self.assertNotIn(comm.AppName, result[1])
+        self.assertEquals(0, packstatus[0])
+        print "Generate APK ----------------> OK!"
+        result = commands.getstatusoutput("ls")
+        self.assertIn(comm.AppName, result[1])
         os.remove(comm.AppName)
-        os.chdir("..")
+        os.chdir("../..")
 
     def test_manifest3(self):
         comm.setUp()
-        os.chdir("resource")
-        cmd = "python %smake_apk.py --package=org.hello.world --arch=%s --mode=%s --manifest=%s" % \
+        os.chdir(comm.ConstPath + "/../testapp/example")
+        cmd = "python %smake_apk.py --package=org.xwalk.example --arch=%s --mode=%s --manifest=%s" % \
         (comm.Pck_Tools, comm.ARCH, comm.MODE, comm.MANIFEST_PATH[2])
         packstatus = commands.getstatusoutput(cmd)
-        if packstatus[0] == 0:
-            print "Generate APK ----------------> OK!"
-            result = commands.getstatusoutput("ls")
-            self.assertIn(comm.AppName, result[1])
-        else:
-            print "Generate APK ----------------> Error!"
-            result = commands.getstatusoutput("ls")
-            self.assertNotIn(comm.AppName, result[1])
+        self.assertEquals(0, packstatus[0])
+        print "Generate APK ----------------> OK!"
+        result = commands.getstatusoutput("ls")
+        self.assertIn(comm.AppName, result[1])
         os.remove(comm.AppName)
 
 if __name__ == '__main__':  
