88e48fbd2ed42d41b26175428c7035daa8729e1f
whj <hongjuanx.wang@intel.com>
2015-02-26 16:32:19 +0800

    [usecase-wrt] Update test description for SharedModeGooglePlay
    Impacted TCs num(approved): New 0, Update 1, Delete 0
    Unit test Platform: Android
    Android test result summary: Pass 1, Fail 0, Block 0

--- usecase/usecase-wrt-android-tests/steps/SharedModeGooglePlay/step.js
+++ usecase/usecase-wrt-android-tests/steps/SharedModeGooglePlay/step.js
@@ -2,7 +2,7 @@ var step = '<p>Purpose:</p>'
             +'<p>Verify that the shared mode app can work well on google play and the UI display normally</p>'
             +'<p>Expected Results:</p>'
             +'<ol>'
-            +'<li>Launch the app successfully, start download crosswalk runtime library, the screen is normally whether orientation or portrait</li>'
+            +'<li>Start download crosswalk runtime library, the screen is normally whether orientation or portrait</li>'
             +'<li>Install crosswalk runtime library successfully</li>'
             +'<li>Display crosswalk main page</li>'
             +'</ol>';
