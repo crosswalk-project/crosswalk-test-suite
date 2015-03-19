d4396d222d82f01a79378e23b3ab3860e9d51143
Li, Hao <haox.li@intel.com>
2015-03-19 11:48:21 +0800

    [usecase][xwalk-3627] Fix the write file issue of filesystem
    -Update the test step info
    -Update the savefile function
    -Remove jquery attribute which is not support on android platform
    
    Impacted TCs num(approved): New 0, Update 1, Delete 0
    Unit test Platform: Android
    Android test result summary: Pass 1, Fail 0, Block 0

--- usecase/usecase-webapi-xwalk-tests/steps/FileSystem/step.js
+++ usecase/usecase-webapi-xwalk-tests/steps/FileSystem/step.js
@@ -1,9 +1,9 @@
 var step = '<p>Test Purpose: </p>'
-          +'<p>Verifies the file can be showed and updated which you select</p>'
+          +'<p>Verifies the file can be showed and can write new content to a file</p>'
           +'<p>Test Steps: </p>'
           +'<ol>'
             +'<li>Click the \'select file\'</li>'
             +'<li>Click the \'write file\'</li>'
           +'</ol>'
           +'<p>Expected Result: </p>'
-          +'<p>Test passes if get and update the local file successfully</p>';
+          +'<p>Test passes if can get the local file and write new content</p>';
