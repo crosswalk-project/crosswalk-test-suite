02a01aa61d7e041938f41a32c340fbdad75ae0ad
Xu Kang <kangx.xu@intel.com>
2015-04-23 11:18:43 +0000

    [usecase-linux][xwalk-3690] Update usecase for open external URL
    Impacted tests(approved): new 0, update 1, delete 2
    Unit test platform: [Linux]
    Unit test result summary: pass 0, fail 1, block 0
    
    Comments:
    
    When the URL is the same with scope section value in manifest, URL should be opened in
    app window self, but now it was opened in browse.

--- usecase/usecase-wrt-linux-tests/samples/ExternalURL_MatchPatterns/README.md
+++
@@ -1,6 +0,0 @@
-## Usecase Design
-
-Purpose:
-
-* Validate open the URL in app windows itself when the xwalk_hosts field with match patterns * in manifest
-
---
+++ usecase/usecase-wrt-linux-tests/steps/ExternalURL/step.js
@@ -0,0 +1,4 @@
+var step = '<font class="fontSize">'
+            +'<p>Purpose:</p>'
+            +'<p>Validate open external URL in browser support</p>'
+          +'</font>';
