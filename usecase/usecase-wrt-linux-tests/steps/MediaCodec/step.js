1844a5fbe88ca2e26560e960c96779e460f9f076
Xu Kang <kangx.xu@intel.com>
2015-04-24 10:03:13 +0000

    [usecase-wrt][xwalk-3705] Add usecase for loading media codec
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: [Linux][Crosswalk 14.43.337]
    Unit test result summary: pass 1, fail , block 0

---
+++ usecase/usecase-wrt-linux-tests/steps/MediaCodec/step.js
@@ -0,0 +1,4 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Purpose: </p>'
+            +'<p>Verifies demonstrates support for loading proprietary media codec</p>'
+          +'</font>';
