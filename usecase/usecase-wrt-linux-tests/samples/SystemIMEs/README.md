563de87446a1a9a1eeb39a89a51f01101d629742
Xu Kang <kangx.xu@intel.com>
2015-05-14 10:45:03 +0000

    [usecase-linux] Add usecase for system IMEs
    Spec Link: https://crosswalk-project.org/jira/browse/XWALK-3823
    
    Mark usecase status as designed due to feature isn't resolved

---
+++ usecase/usecase-wrt-linux-tests/samples/SystemIMEs/README.md
@@ -0,0 +1,7 @@
+## Usecase Design
+
+This sample demonstrates system IMEs support for chinese.
+
+## Spec Link
+
+https://crosswalk-project.org/jira/browse/XWALK-3823
---
+++ usecase/usecase-wrt-linux-tests/steps/SystemIMEs/step.js
@@ -0,0 +1,4 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Purpose:</p>'
+            +'<p>Verifies system IMEs support for chinese</p>'
+          +'</font>';
