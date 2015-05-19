358d5ba4db7f960913eaaa72feadd7fb5add4755
yang-yunlong <yunlongx.yang@intel.com>
2015-05-19 13:39:41 +0800

    Move new feature cases to v5

--- embeddingapi/embedding-api-android-tests/embeddingapi/src/org/xwalk/embedding/test/v5/XWalkViewTest.java
+++ embeddingapi/embedding-api-android-tests/embeddingapi/src/org/xwalk/embedding/test/v5/XWalkViewTest.java
@@ -4,6 +4,7 @@
 
 package org.xwalk.embedding.test.v5;
 
+import android.graphics.Color;
 import org.xwalk.embedding.base.XWalkViewTestBase;
 import android.annotation.SuppressLint;
 import android.test.suitebuilder.annotation.SmallTest;
@@ -23,6 +24,109 @@ public class XWalkViewTest extends XWalkViewTestBase {
             loadDataSync(null, EMPTY_PAGE, "text/html", false);
             String result = executeJavaScriptAndWaitForResult("navigator.userAgent;");
             assertEquals(EXPECTED_USER_AGENT, result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    @SmallTest
+    public void testSetZOrderOnTop_True() {
+        try {
+            getInstrumentation().runOnMainSync(new Runnable() {
+
+                @Override
+                public void run() {
+                    mXWalkView.setZOrderOnTop(true);
+                }
+            });
+            assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    @SmallTest
+    public void testSetZOrderOnTop_False() {
+        try {
+            getInstrumentation().runOnMainSync(new Runnable() {
+
+                @Override
+                public void run() {
+                    mXWalkView.setZOrderOnTop(false);
+                }
+            });
+            assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+    
+    @SmallTest
+    public void testSetBackgroundColor_Color() {
+        try {
+            getInstrumentation().runOnMainSync(new Runnable() {
+
+                @Override
+                public void run() {
+                	mXWalkView.setBackgroundColor(Color.RED);
+                }
+            });
+            assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    @SmallTest
+    public void testSetBackgroundColor_Value() {
+        try {
+            getInstrumentation().runOnMainSync(new Runnable() {
+
+                @Override
+                public void run() {
+                	mXWalkView.setBackgroundColor(Color.parseColor("#00FF00"));
+                }
+            });
+            assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+    
+    @SmallTest
+    public void testSetBackgroundColor_Transparent() {
+        try {
+            getInstrumentation().runOnMainSync(new Runnable() {
+
+                @Override
+                public void run() {
+                	mXWalkView.setBackgroundColor(0);
+                }
+            });
+            assertTrue(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+    
+    @SmallTest
+    public void testSetXWalkViewTransparent() {
+        try {
+            getInstrumentation().runOnMainSync(new Runnable() {
+
+                @Override
+                public void run() {
+                	mXWalkView.setZOrderOnTop(true);
+                	mXWalkView.setBackgroundColor(0);
+                }
+            });
+            assertTrue(true);
         } catch (Exception e) {
             e.printStackTrace();
             assertTrue(false);
