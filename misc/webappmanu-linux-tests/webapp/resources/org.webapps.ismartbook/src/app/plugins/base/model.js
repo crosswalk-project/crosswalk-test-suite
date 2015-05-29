0f34b537c6391f566612af5269e1b2331de27ff6
Xu Kang <kangx.xu@intel.com>
2015-05-29 17:13:41 +0000

    [misc-linux] Remove tests for iSmartBook
    Source code link(https://github.com/webup/reader.git)
    for iSmartBook is invalid.
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-3951

--- misc/webappmanu-linux-tests/webapp/resources/org.webapps.ismartbook/src/app/plugins/base/model.js
+++
@@ -1,9 +0,0 @@
-/**
- * 控件基础Model类
- * 目前只是简单的返回Backbone的Model
- * 
- */
-define(function (require, exports, module) {
-  var Backbone = require("./backbone.extend");
-  return Backbone.Model.extend({});
-});
