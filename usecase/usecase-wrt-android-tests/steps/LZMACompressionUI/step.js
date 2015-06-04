f6fe4825e306c57c8ad7b77633b55d16f40ba52b
wanghongjuan <hongjuanx.wang@intel.com>
2015-06-04 18:30:53 +0800

    [usecase] Update the description for downloadsimul
    - Update downloadsimul description
    - Update the format for LZMACompressionUI
    
    Failure analysis:
    https://crosswalk-project.org/jira/browse/XWALK-4326
    
    Impacted TCs num(approved): New 0, Update 2, Delete 0
    Unit test Platform: Android
    Android test result summary: Pass 1, Fail 1, Block 0

--- usecase/usecase-wrt-android-tests/steps/LZMACompressionUI/step.js
+++ usecase/usecase-wrt-android-tests/steps/LZMACompressionUI/step.js
@@ -3,6 +3,6 @@ var step = '<p>Purpose:</p>'
             +'<p>Expected Results:</p>'
             +'<ol>'
             +'<li>The size is smaller than normal apk that build without enable-lzma parameters</li>'
-            +'<li>Find a file named like: libxwalkcore.so.lzma under the directory res/raw of the extracting folder</li>'
-            +'<li>Display message just like :Decompressing the Crosswalk Project runtime, open the crosswalk main page without crash or other display issue after complate decompressing</li>'
+            +'<li>Find a file named like: libxwalkcore.so.* under the directory res/raw of the extracting folder</li>'
+            +'<li>Display message just like: Decompressing the Crosswalk Project runtime, open the crosswalk main page without crash or other display issue after complate decompressing</li>'
             +'</ol>';
