3e948cd1031298ce770010b4f13020c0d4049ba0
wanghongjuan <hongjuanx.wang@intel.com>
2015-06-11 13:50:02 +0800

    [Crosswalk-14][usecase] Add test for upgradelib
    - Add upgrade test
    - Update test description for DownloadUsage/res/downloadsimul
    
    Impacted TCs num(approved): New 1, Update 0, Delete 0
    Unit test Platform: Android
    Android test result summary: Pass 1, Fail 0, Block 0

--- usecase/usecase-wrt-android-tests/steps/DownloadUsage/res/downloadsimul/step.js
+++ usecase/usecase-wrt-android-tests/steps/DownloadUsage/res/downloadsimul/step.js
@@ -1,9 +1,11 @@
 var step = '<p>Test Purpose: </p>'
-            +'<p>Verifies the Crosswalk Runtime Library can not download from googleplay both in two sharedmode apps at the same time</p>'
+            +'<p>Verify the Crosswalk Runtime Library can download twice with config url method in two sharedmode apps at the same time(test for config url)</p>'
+            +'<p>Verify the Crosswalk Runtime Library can not download twice from google play in two sharedmode apps at the same time(test for google play)</p>'
             +'<p>Expected Results: </p>'
             +'<ol>'
             +'<li>Launch web app failed and prompt: Please get Crosswalk Runtime Library</li>'
             +'<li>Start to download the Crosswalk Runtime Library</li>'
             +'<li>Launch web app failed and prompt: Please get Crosswalk Runtime Library</li>'
-            +'<li>The app can not download twice time at the same time, display the downloading progress bar at the first time</li>'
+            +'<li>The app can download twice at the same time(config url)</li>'
+            +'<li>The app can not download twice at the same time, display the downloading progress bar at the first time(google play)</li>'
             +'</ol>';
