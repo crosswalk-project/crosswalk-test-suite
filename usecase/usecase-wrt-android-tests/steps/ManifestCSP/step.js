799ea89cccfee67db55bded1fe03277e9eb3f7ee
wanghongjuan <hongjuanx.wang@intel.com>
2015-06-26 14:40:11 +0800

    [usecase-wrt] Add test for CSP to usecase
    Impacted TCs num(approved): New 1, Update 0, Delete 0
    Unit test Platform: Android
    Android test result summary: Pass 1, Fail 0, Block 0

---
+++ usecase/usecase-wrt-android-tests/steps/ManifestCSP/step.js
@@ -0,0 +1,6 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verify the the csp directive that member of the manifest can work well</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>All descriptions in the test app are correct</li>'
+            +'</ol>';
