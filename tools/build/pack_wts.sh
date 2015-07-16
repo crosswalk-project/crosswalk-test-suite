18cfd7248e849112ad4d4899021d84abc41ed7f7
qiuzhong <zhongx.qiu@intel.com>
2015-07-16 18:37:53 +0000

    [WTS-pack] Update pack_wts.sh to add usecase test
    Update pack_wts.sh to add usecase test suite and enhance script.
    Add -a option to specify the commit ID (default is the latest commit ID)

--- tools/build/pack_wts.sh
+++ tools/build/pack_wts.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#
+# Modified by: Qiu Zhong <zhongx.qiu@intel.com>
+# 2015-07-16: Add a commit id option to make it more fexible to call this script.
+#
+
+usage="Usage: ./pack_wts.sh [-a <commit id>]
+-a <commit id> The 40-bit SHA-1 commit ID in git when commit
+               It's optional, if this option is null, use the default Release_ID of latest commit id.
+"
+
 home=$PWD
 
 # We'll put the script to crosswalk-test-suite/tools/build/
@@ -9,10 +19,31 @@ home=$PWD
 # after
 crosswalk_test_suite_dir=$home/../..
 work=${crosswalk_test_suite_dir}/webapi
+usecase_dir=$crosswalk_test_suite_dir/usecase
+demoex_dir=$crosswalk_test_suite_dir/../release/demo-express
 
+# default release ID
+release_ID=`git log --pretty=oneline -1|awk '{print $1}'`
+
+while getopts a:h opt
+do
+    case "$opt" in
+    a) release_ID=$OPTARG;;
+    *) echo "$usage"
+    	exit 1;;
+    esac    
+done
+
+cd $demoex_dir;git reset --hard HEAD;git checkout master;git pull
+cd $work/../;git reset --hard HEAD;git clean -dfx;git checkout master;git fetch upstream;git rebase upstream/master;git reset --hard $release_ID
+
+cp -dpRv $demoex_dir/samples/* $usecase_dir/usecase-webapi-xwalk-tests/samples/
+cp -dpRv $demoex_dir/res/* $usecase_dir/usecase-webapi-xwalk-tests/res/
+#cp -dpRv $demoex_dir/samples-wrt/* $usecase_dir/usecase-wrt-android-tests/samples/
 rm -rf *zip /tmp/tests
 mkdir -p /tmp/tests/common
 mkdir -p /tmp/tests/resources
+python $work/../tools/build/copy.py
 
 #Add version.json file
 export LANG="en_US"
@@ -24,7 +55,7 @@ cat $home/test_suites.list | while read suite
 do
     ls $suite &>/dev/null
     if [ $? -ne 0 ];then
-        echo "$suite not found"
+        echo "$suite not found" >> $home/nofound.txt
         continue
     fi
 
@@ -52,9 +83,33 @@ do
     test -d $suite/resources && cp -rf $suite/resources/* /tmp/tests/resources
 done
 
+mkdir /tmp/tests/usecase
+#mkdir /tmp/tests/usecase-wrt
+
+cp -dpRv $usecase_dir/usecase-webapi-xwalk-tests/icon.png /tmp/tests/usecase/
+cp -dpRv $usecase_dir/usecase-webapi-xwalk-tests/res /tmp/tests/usecase/
+cp -dpRv $usecase_dir/usecase-webapi-xwalk-tests/steps /tmp/tests/usecase/
+cp -dpRv $usecase_dir/usecase-webapi-xwalk-tests/samples /tmp/tests/usecase/
+cp -dpRv $usecase_dir/usecase-webapi-xwalk-tests/manifest.json /tmp/tests/usecase/
+cp -dpRv $usecase_dir/usecase-webapi-xwalk-tests/tests.android.xml /tmp/tests/usecase/tests.xml
+cp -dpRv $work/../tools/bootstrap-fw/* /tmp/tests/usecase/
+
+#cp -dpRv $usecase_dir/usecase-wrt-android-tests/tests.android.xml /tmp/tests/usecase-wrt/tests.xml
+#cp -dpRv $usecase_dir/usecase-wrt-android-tests/steps /tmp/tests/usecase-wrt/
+#cp -dpRv $work/../tools/bootstrap-fw/* /tmp/tests/usecase-wrt
+
+
+
+#rm Makefile
+find /tmp/tests -name "Makefile*" |xargs -I% rm -rf %
+
 #zip file
 cd /tmp/tests/
 rm -fr ./typedarrays/tools
+## TODO: this will remove after the redirect.py has been fixed
+#grep redirect.py -rl *|xargs -I% rm %
+#find . -name "redirect.py" -delete
+## end
 
 ## TODO: move qunit to manual
 
@@ -74,3 +129,7 @@ mv tests-${version}.wts.zip $home
 
 #clean environment
 rm -rf /tmp/tests
+
+cd $work/../
+git reset --hard HEAD
+git clean -dfx
