25b643d856dd7e240d3f430b8c5a0c62edd3c22b
yang-yunlong <yunlongx.yang@intel.com>
2015-08-18 13:47:30 +0800

    [Embeddingapi] Modify async project test and xmls names
    -Modify async project test and xmls names
    -Add 'Async' to the end fo case name and other related files.
    
    Impacted tests(approved): new 0, update 50, delete 0
    Unit test platform: [Android]
    Unit test result summary: pass 50, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-4823

---
+++ embeddingapi/embedding-asyncapi-android-tests/embeddingapi/src/org/xwalk/embedding/test/v5/XWalkResourceClientTestAsync.java
@@ -0,0 +1,47 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.xwalk.embedding.test.v5;
+
+
+import org.xwalk.core.ClientCertRequestHandler;
+import org.xwalk.embedding.base.OnDocumentLoadedInFrameHelper;
+import org.xwalk.embedding.base.OnReceivedClientCertRequestHelper;
+import org.xwalk.embedding.base.XWalkViewTestBase;
+import org.xwalk.embedding.util.CommonResources;
+
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+
+public class XWalkResourceClientTestAsync extends XWalkViewTestBase {
+
+    @SmallTest
+    public void testAOnDocumentLoadedInFrame() throws Throwable {
+        OnDocumentLoadedInFrameHelper mOnDocumentLoadedInFrameHelper = mTestHelperBridge.getOnDocumentLoadedInFrameHelper();
+        String path = "/test.html";
+        String pageContent = CommonResources.makeHtmlPageFrom("<title>Test</title>",
+                "<div> The title is: Test </div>");
+        final String url = addPageToTestServer(mWebServer, path, pageContent);
+
+        loadUrlSync(url);
+        assertEquals(1, mOnDocumentLoadedInFrameHelper.getFrameId());
+        assertEquals(1, mOnDocumentLoadedInFrameHelper.getCallCount());
+    }
+    
+    @MediumTest
+    public void testClientCertRequest() throws Throwable {
+    	OnReceivedClientCertRequestHelper mOnReceivedClientCertRequestHelper = mTestHelperBridge.getOnReceivedClientCertRequestHelper();    	
+    	final String url = "https://egov.privasphere.com/";
+    	int onReceivedClientCertRequestCallCount = mOnReceivedClientCertRequestHelper.getCallCount();
+    	try {
+    	    loadUrlAsync(url);
+    	    mOnReceivedClientCertRequestHelper.waitForCallback(onReceivedClientCertRequestCallCount);
+    	    assertEquals(ClientCertRequestHandler.class.getName(), mOnReceivedClientCertRequestHelper.getHandler().getClass().getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+    	}
+    }    
+}
+
---
+++ usecase/usecase-embedding-android-tests/embeddingapi-async/src/org/xwalk/embedded/api/asyncsample/XWalkWithSaveStateAsync.java
@@ -0,0 +1,95 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.xwalk.embedded.api.asyncsample;
+
+import android.app.Activity;
+import org.xwalk.core.XWalkInitializer;
+import org.xwalk.core.XWalkView;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class XWalkWithSaveStateAsync extends Activity implements XWalkInitializer.XWalkInitListener {
+    private XWalkView xWalkView;
+    private XWalkInitializer mXWalkInitializer;
+    private Button button_one;
+    private Button button_two;
+    private Button button_three;
+    private FrameLayout mFrameLayout;
+    private Bundle bundle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mXWalkInitializer = new XWalkInitializer(this, this);
+        mXWalkInitializer.initAsync();
+    }
+
+    @Override
+    public final void onXWalkInitStarted() {
+        // It's okay to do nothing
+    }
+
+    @Override
+    public final void onXWalkInitCancelled() {
+        // It's okay to do nothing
+    }
+
+    @Override
+    public final void onXWalkInitFailed() {
+        // Do crash or logging or anything else in order to let the tester know if this method get called
+    }
+
+    @Override
+    public final void onXWalkInitCompleted() {
+        StringBuffer mess = new StringBuffer();
+        mess.append("Test Purpose: \n\n")
+        .append("Verifies XWalkView can saveState & restoreState bundle.\n\n")
+        .append("Test  Step:\n\n")
+        .append("1. Click saveInstance button when finish loading baidu home page.\n")
+        .append("2. Click load_other button and meituan homepage shows.\n")
+        .append("3. Click restoreInstance button and wait page changing to first one.\n\n")  
+        .append("Expected Result:\n\n")
+        .append("Test passes if app show baidu home page.");
+        new  AlertDialog.Builder(this)
+        .setTitle("Info" )
+        .setMessage(mess.toString())
+        .setPositiveButton("confirm" ,  null )
+        .show();
+        
+        setContentView(R.layout.savestate_layout);
+        button_one = (Button) findViewById(R.id.button_one);
+        button_two = (Button) findViewById(R.id.button_two);
+        button_three = (Button) findViewById(R.id.button_three);
+        mFrameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+        
+        bundle = new Bundle();
+        xWalkView = new XWalkView(this,this);
+        xWalkView.load("http://www.baidu.com", null);
+        button_one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xWalkView.saveState(bundle);
+            }
+        });
+        button_two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xWalkView.load("http://i.meituan.com", null);
+            }
+        });
+        button_three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xWalkView.restoreState(bundle);
+            }
+        });
+        mFrameLayout.addView(xWalkView);
+    }
+}
