f05ae12ae8719a39485f19368bf1f43c2a30443a
zhuyongyong <yongyongx.zhu@intel.com>
2015-09-23 13:42:07 +0800

    [usecase-windows] Add icon case for usecase-wrt-windows-tests
    - add playing video from http:// for VideoPlay
    - Failed Analyse: XWALK-4998 XWALK-4960
    
    Impacted tests(approved): new 1, update 1, delete 0
    Unit test platform: Crosswalk Project for Windows 15.44.374.0
    Unit test result summary: pass 0, fail 2, block 0

---
+++ usecase/usecase-wrt-windows-tests/steps/PackertoolIcon/step.js
@@ -0,0 +1,7 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verify that the specified icon is shown in "Apps by name" and the status bar</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>This web app icon is shown in "Apps by name" page and status bar</li>'
+            +'<li>This web app icon is the same as the following picture</li>'
+            +'</ol>';
--- usecase/usecase-wrt-windows-tests/steps/VideoPlay/step.js
+++ usecase/usecase-wrt-windows-tests/steps/VideoPlay/step.js
@@ -1,5 +1,5 @@
 var step = '<p>Purpose:</p>'
-            +'<p>Verify that the play video file from file:/// normally</p>'
+            +'<p>Verify that the play video file from file:/// and http:// can play normally</p>'
             +'<p>Expected Result:</p>'
             +'<ol>'
             +'<li>The videos can play normally</li>'
