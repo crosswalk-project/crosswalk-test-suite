3ba49fb185e626389be9dceae9c43c02541dc0d5
wanghongjuan <hongjuanx.wang@intel.com>
2015-11-23 11:05:32 +0800

    [usecase]Update tests for apptools replace make_apk.py to build test suite
    - Fail BUG/Feature ID: XWALK-5820/3108/3330/5100/4480/5094/5812/5813
    
    - Block BUG/Feature ID: XWALK-5646/4248/5591
    
    Impacted TCs num(approved): New 0, Update 97, Delete 6
    Unit test Platform: Android Crosswalk Canary 17.45.437.0
    Android test result summary: Pass 61, Fail 11, Block 24
    
    https://crosswalk-project.org/jira/browse/XWALK-5716
    https://crosswalk-project.org/jira/browse/XWALK-5724
    https://crosswalk-project.org/jira/browse/XWALK-5726
    https://crosswalk-project.org/jira/browse/XWALK-5760
    https://crosswalk-project.org/jira/browse/XWALK-5729
    https://crosswalk-project.org/jira/browse/XWALK-5732
    https://crosswalk-project.org/jira/browse/XWALK-5734
    https://crosswalk-project.org/jira/browse/XWALK-5759
    https://crosswalk-project.org/jira/browse/XWALK-5799
    https://crosswalk-project.org/jira/browse/XWALK-5807

--- usecase/usecase-wrt-android-tests/steps/ManifestDemo8/step.js
+++
@@ -1,8 +0,0 @@
-var step = '<p>Purpose:</p>'
-            +'<p>Verify the manifest member xwalk_view can work normally when launch the test app</p>'
-            +'<p>Expected Results:</p>'
-            +'<ol>'
-            +'<li>Install web app successfully</li>'
-            +'<li>Launch web app successfully, no crash and error message</li>'
-            +'<li>The background color of the test page and splash screen is blue and display some descriptions about manifest configuration </li>'
-            +'</ol>';
