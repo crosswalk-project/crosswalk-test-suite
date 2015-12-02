5db4436aa33b248bc504a4d4cac60f106494b9e8
zhuyongyong <yongyongx.zhu@intel.com>
2015-12-02 13:48:31 +0800

    [windows] Add picker usecase for usecase-wrt-windows-tests
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: Crosswalk Project for windows 17.45.426.0
    Unit test result summary: pass 1, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-5435

---
+++ usecase/usecase-wrt-windows-tests/samples/Picker/README.md
@@ -0,0 +1,6 @@
+## Usecase Design
+
+This sample demonstrates Picker feature basic functionalities, include:
+
+* selecte a file from device
+
---
+++ usecase/usecase-wrt-windows-tests/steps/Picker/step.js
@@ -0,0 +1,6 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verify that the file from device can be selected</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>File can be selected success, and file name display correctly</li>'
+            +'</ol>';
