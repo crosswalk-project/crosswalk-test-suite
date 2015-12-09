2cbfa5b6af68f9a80060d269a54d5797313d4266
zhuyongyong <yongyongx.zhu@intel.com>
2015-12-09 14:57:36 +0800

    [Windows] Remove leave fullscreen step for wrt fullscreen case
    Impacted tests(approved): new 0, update 1, delete 0
    Unit test platform: Crosswalk Project for windows 17.45.426.0
    Unit test result summary: pass 1, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-5502

--- usecase/usecase-wrt-windows-tests/steps/Fullscreen/step.js
+++ usecase/usecase-wrt-windows-tests/steps/Fullscreen/step.js
@@ -1,7 +1,6 @@
 var step = '<p>Purpose:</p>'
-            +'<p>Verify that user can enter and exit the fullscreen</p>'
+            +'<p>Verify that the app can launch in fullscreen state</p>'
             +'<p>Expected Result:</p>'
             +'<ol>'
             +'<li>The app run normally and on fullscreen state</li>'
-            +'<li>The app exit fullscreen successfully</li>'
             +'</ol>';
