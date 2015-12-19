e1610f0b077112446e0389b9f2cf599771d003de
chunywang <chunyanx.wang@intel.com>
2015-12-19 17:44:30 +0800

    [usecase] Add Set-Cookie test
    The XMLHttpRequest API need docroot for usecase in tinyweb, it is blocked by
    https://crosswalk-project.org/jira/browse/XWALK-5905.
    
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: Crosswalk Project for Android 18.46.455.0
    Unit test result summary: pass 0, fail 0, block 1
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-5985

---
+++ usecase/usecase-webapi-xwalk-tests/steps/SetCookie/step.js
@@ -0,0 +1,8 @@
+var step = '<font class="fontSize">'
+         + '<p>Test Purpose: </p>'
+         + '<p>Verifies HTTP response header "Set-Cookie".</p>'
+         + '<p>Test steps: </p>'
+         + '<p>Check the "Set-Cookie" value.</p>'
+         + '<p>Expected Result: </p>'
+         + '<p>Test passes if it should get the response header value expect "Set-Cookie", the value of "Set-Cookie" should be null.</p>'
+         + '</font>'
