c4fad31de26931268cd70e1fa15fff47445fee4a
Li Hao <haox.li@intel.com>
2015-12-22 13:13:31 +0800

    [wrt] Test for input password echoing
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: Crosswalk Project for Android 18.46.453.0
    Unit test result summary: pass 1, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-4306

---
+++ usecase/usecase-wrt-android-tests/steps/InputPassword/step.js
@@ -0,0 +1,6 @@
+var step = '<font class="fontSize">'
+         + '  <p>Test Purpose: </p>'
+         + '  <p>Verifies Password field displays character before masking.</p>'
+         + '  <p>Expected Result: </p>'
+         + '  <p>Test passes if the character is visible briefly before masking when type a character in password field.</p>'
+         + '</font>';
