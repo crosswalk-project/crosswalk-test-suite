784f6dd396d8d3f62f71e703dd14e4073aa2364f
Li Hao <haox.li@intel.com>
2016-01-13 19:22:12 +0800

    [Crosswalk-17][usecase-wrt] Remove ChromeProtocol due to chrome protocol deprecated
    chrome:// is not a required feature for crosswalk runtime.
    
    https://crosswalk-project.org/jira/browse/XWALK-3595

--- usecase/usecase-wrt-android-tests/steps/ChromeProtocol/step.js
+++
@@ -1,6 +0,0 @@
-var step = '<p>Purpose:</p>'
-            +'<p>Verify the app can be back to home page when press "Back" key from "chrome://gpu" link</p>'
-            +'<p>Expected Result:</p>'
-            +'<ol>'
-            +'<li>Go back to home page</li>'
-            +'</ol>';
