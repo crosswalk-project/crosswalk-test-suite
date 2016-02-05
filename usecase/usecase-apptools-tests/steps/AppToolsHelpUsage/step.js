75640b78f74fbe80e7e8d8477c8ae21c698154bf
Liu Yun <yunx.liu@intel.com>
2016-02-05 15:36:18 +0800

    [usecase-apptools] Move manual tests to apptools usecase
    Impacted tests(approved): new 0, update 20, delete 0
    Unit test platform: [Android]
    Unit test result summary: pass 20, fail 0, block 0
    
    https://crosswalk-project.org/jira/browse/XWALK-5797

---
+++ usecase/usecase-apptools-tests/steps/AppToolsHelpUsage/step.js
@@ -0,0 +1,6 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verifies if all usages of crosswalk-app and crosswalk-pkg work well as description</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>All usages work well as description</li>'
+            +'</ol>';
