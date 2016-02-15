807fd1998d20cab92290278466f2937fefd7ab26
zhuyongyong <yongyongx.zhu@intel.com>
2016-02-15 15:56:07 +0800

    [Windows] Add usecase for Geolocation Permission control and Widevine
    Impacted tests(approved): new 2, update 0, delete 0
    Unit test platform: Crosswalk Project for Windows 18.46.457.0
    Unit test result summary: pass 2, fail 0, block 0

---
+++ usecase/usecase-wrt-windows-tests/steps/Widevine/step.js
@@ -0,0 +1,7 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verify the crosswalk support widevine</p>'
+            +'<p>Expected Results:</p>'
+            +'<ol>'
+            +'<li>Install web app successfully</li>'
+            +'<li>Launch web app successfully, no crash and error message and the video can play normally(maybe need to wait a moment)</li>'
+            +'</ol>';
