8dd961fc3943762a015b512c1425e245eb42496f
yang-yunlong <yunlongx.yang@intel.com>
2016-03-02 15:27:56 +0800

    [EmbeddingAPI] add autocase for API clearSslPreferences()
    -Add autocase for API clearSslPreferences()
    --Cover the XWalkActivity and XWalkInitializer two ways
    
    Impacted tests(approved): new 2, update 0, delete 0
    Unit test platform: [Android]
    Unit test result summary: pass 2, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-6438

---
+++ embeddingapi/embedding-api-android-tests/embeddingapi/src/org/xwalk/embedding/test/v6/ClearSslPreferenceTest.java
@@ -0,0 +1,68 @@
+// Copyright (c) 2014 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.xwalk.embedding.test.v6;
+
+import org.chromium.net.test.util.TestWebServer;
+import org.xwalk.embedding.base.OnReceivedSslHelper;
+import org.xwalk.embedding.base.XWalkViewTestBase;
+import android.annotation.SuppressLint;
+import android.test.suitebuilder.annotation.SmallTest;
+
+
+@SuppressLint("NewApi")
+public class ClearSslPreferenceTest extends XWalkViewTestBase {
+    private TestWebServer mWebServer;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mWebServer = TestWebServer.startSsl();
+    }
+
+    @SmallTest
+    // If the user allows the ssl error, the same ssl error will not trigger
+    // the onReceivedSslError callback; If the user denies it, the same ssl
+    // error will still trigger the onReceivedSslError callback.
+    public void testSslPreferences() throws Throwable {
+        final String pagePath = "/hello.html";
+        final String pageUrl =
+                mWebServer.setResponse(pagePath, "<html><body>hello world</body></html>", null);
+        final OnReceivedSslHelper onReceivedSslErrorHelper =
+                mTestHelperBridge.getOnReceivedSslHelper();
+        int onSslErrorCallCount = onReceivedSslErrorHelper.getCallCount();
+
+        loadUrlSync(pageUrl);
+
+        assertEquals(onSslErrorCallCount + 1, onReceivedSslErrorHelper.getCallCount());
+        assertEquals(1, mWebServer.getRequestCount(pagePath));
+
+        // Now load the page again. This time, we expect no ssl error, because
+        // user's decision should be remembered.
+        onSslErrorCallCount = onReceivedSslErrorHelper.getCallCount();
+        loadUrlSync(pageUrl);
+        assertEquals(onSslErrorCallCount, onReceivedSslErrorHelper.getCallCount());
+
+        // Now clear the ssl preferences then load the same url again. Expect to see
+        // onReceivedSslError getting called again.
+        clearSslPreferences();
+        onSslErrorCallCount = onReceivedSslErrorHelper.getCallCount();
+        loadUrlSync(pageUrl);
+        assertEquals(onSslErrorCallCount + 1, onReceivedSslErrorHelper.getCallCount());
+
+        // Now clear the stored decisions and tell the client to deny ssl errors.
+        clearSslPreferences();
+        setAllowSslError(false);
+        onSslErrorCallCount = onReceivedSslErrorHelper.getCallCount();
+        loadUrlSync(pageUrl);
+        assertEquals(onSslErrorCallCount + 1, onReceivedSslErrorHelper.getCallCount());
+
+        // Now load the same page again. This time, we still expect onReceivedSslError,
+        // because we only remember user's decision if it is "allow".
+        onSslErrorCallCount = onReceivedSslErrorHelper.getCallCount();
+        loadUrlSync(pageUrl);
+        assertEquals(onSslErrorCallCount + 1, onReceivedSslErrorHelper.getCallCount());
+    }
+}
