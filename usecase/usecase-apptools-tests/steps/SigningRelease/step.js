36f7de1599448dcf7e60a23be84404ed891d1a2e
Liu Yun <yunx.liu@intel.com>
2016-03-10 14:40:07 +0800

    [usecase-apptools] Add tests to test signing release for android apptools usecase
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: [Android]
    Unit test result summary: pass 1, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-5436

---
+++ usecase/usecase-apptools-tests/steps/SigningRelease/step.js
@@ -0,0 +1,6 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verifies if App Tool can support to sign the building release APKs</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>The building release APKs can be signed successfully, the signing release APKs can be installed and launched</li>'
+            +'</ol>';
