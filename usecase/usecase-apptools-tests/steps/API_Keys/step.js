11f39c0afbe2329311bf8b77572249f159fb8bb9
Liu Yun <yunx.liu@intel.com>
2016-03-10 20:11:33 +0800

    [usecase-apptools] Add tests to test API keys for windows apptools usecase
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: [Windows]
    Unit test result summary: pass 1, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-6497

---
+++ usecase/usecase-apptools-tests/steps/API_Keys/step.js
@@ -0,0 +1,6 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verifies App Tool support API keys</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>The keys are in the windows registry under HKEY_CURRENT_USER/Software/org.test/org.test.foo</li>'
+            +'</ol>';
