aebff1cda547671f6707971bc1e0d008a0338045
yang-yunlong <yunlongx.yang@intel.com>
2016-03-31 13:39:47 +0800

    [EmbeddingAPI] Modify usecase of XWalkViewWithRedirection to better test
    -Modify usecase of XWalkViewWithRedirection to better test
    -Cover the XWalkActivity and XWalkInitializer two ways
    
    Impacted tests(approved): new 0, update 2, delete 0
    Unit test platform: [Android]
    Unit test result summary: pass 2, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-6654

--- usecase/usecase-embedding-android-tests/embeddingapi-async/src/org/xwalk/embedded/api/asyncsample/client/XWalkViewWithRedirectionAsync.java
+++ usecase/usecase-embedding-android-tests/embeddingapi-async/src/org/xwalk/embedded/api/asyncsample/client/XWalkViewWithRedirectionAsync.java
@@ -9,72 +9,58 @@ import org.xwalk.core.XWalkView;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class XWalkViewWithRedirectionAsync extends Activity implements XWalkInitializer.XWalkInitListener {
-	private static final String TAG = XWalkViewWithRedirectionAsync.class.getSimpleName();
 
     private XWalkView mXWalkView;
     private XWalkInitializer mXWalkInitializer;
-    private TextView LoadstoppedTimes;
-    private int times_started = 0;
-    private int times_stopped = 0;
-    private int progress = 0;
+    private TextView state;
 
-	class UIClient extends XWalkUIClient{
+    class UIResourceClient extends XWalkResourceClient{
 
-		public UIClient(XWalkView view) {
-			super(view);
-			// TODO Auto-generated constructor stub
-		}
+        public UIResourceClient(XWalkView view) {
+            super(view);
+            // TODO Auto-generated constructor stub
+        }
 
-		@Override
-		public void onPageLoadStopped(XWalkView view, String url,
-				LoadStatus status) {
-			// TODO Auto-generated method stub
-			super.onPageLoadStopped(view, url, status);
-			Log.d(TAG, "onPageLoadStopped called");
-			times_stopped++;
-			updateLoadingState();
-		}
+        @Override
+        public void onProgressChanged(XWalkView view, int progressInPercent) {
+            // TODO Auto-generated method stub
+            super.onProgressChanged(view, progressInPercent);
+            updateLoadingState();
+        }
+    }
 
-		@Override
-		public void onPageLoadStarted(XWalkView view, String url) {
-			// TODO Auto-generated method stub
-			super.onPageLoadStarted(view, url);
-			Log.d(TAG, "onPageLoadStarted called");
-			times_started++;
-			updateLoadingState();
-		}
 
-	}
+    class UIClient extends XWalkUIClient{
 
-	class UIResourceClient extends XWalkResourceClient{
+        public UIClient(XWalkView view) {
+            super(view);
+            // TODO Auto-generated constructor stub
+        }
 
-		public UIResourceClient(XWalkView view) {
-			super(view);
-			// TODO Auto-generated constructor stub
-		}
+        @Override
+        public void onPageLoadStopped(XWalkView view, String url,
+                LoadStatus status) {
+            // TODO Auto-generated method stub
+            super.onPageLoadStopped(view, url, status);
+            updateLoadingState();
+        }
 
-		@Override
-		public void onProgressChanged(XWalkView view, int progressInPercent) {
-			// TODO Auto-generated method stub
-			super.onProgressChanged(view, progressInPercent);
-			Log.d(TAG, "Loading Progress:" + progressInPercent);
-			progress = progressInPercent;
-			updateLoadingState();
-		}
+        @Override
+        public void onPageLoadStarted(XWalkView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageLoadStarted(view, url);
+            updateLoadingState();
+        }
+    }
 
-	}
-
-	private void updateLoadingState() {
-		// TODO Auto-generated method stub
-		String state = "onPageLoadStarted called: " + times_started +
-				       "\nonPageLoadStopped called: " + times_stopped +
-				       "\nonProgressChanged id: " + progress;
-		LoadstoppedTimes.setText(state);
-	}
+    private void updateLoadingState() {
+        // TODO Auto-generated method stub
+        String s = "http://www.cnn.com/ ==> " + mXWalkView.getUrl();
+        state.setText(s);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,21 +90,18 @@ public class XWalkViewWithRedirectionAsync extends Activity implements XWalkInit
 		// TODO Auto-generated method stub
         StringBuffer mess = new StringBuffer();
         mess.append("Test Purpose: \n\n")
-        .append("Load a page with redirection, track onPageLoadStopped, onPageLoadStarted, onProgressChanged called times.\n\n")
-        .append("Test  Step:\n\n")
-        .append("1. load the website http://www.cnn.com/ automatically.\n")
-        .append("2. observe the onProgressChanged id until it is 100.\n\n")
+        .append("Load a page with redirection, track onPageLoadStopped, onPageLoadStarted, onProgressChanged.\n\n")
         .append("Expected Result:\n\n")
-        .append("Test passes if onPageLoadStopped called shows 1 time");
+        .append("Test passes if the message show the redirecting website address");
         new  AlertDialog.Builder(this)
         .setTitle("Info" )
         .setMessage(mess.toString())
         .setPositiveButton("confirm" ,  null )
         .show();
 
-        setContentView(R.layout.version_layout);
+        setContentView(R.layout.activity_xwalk_view_with_redirection_async);
         mXWalkView = (XWalkView) findViewById(R.id.xwalkview);
-        LoadstoppedTimes = (TextView) findViewById(R.id.text1);
+        state = (TextView) findViewById(R.id.message_tv);
 
         mXWalkView.setUIClient(new UIClient(mXWalkView));
         mXWalkView.setResourceClient(new UIResourceClient(mXWalkView));
--- usecase/usecase-embedding-android-tests/embeddingapi/src/org/xwalk/embedded/api/sample/client/XWalkViewWithRedirection.java
+++ usecase/usecase-embedding-android-tests/embeddingapi/src/org/xwalk/embedded/api/sample/client/XWalkViewWithRedirection.java
@@ -9,102 +9,82 @@ import org.xwalk.core.XWalkView;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class XWalkViewWithRedirection extends XWalkActivity{
-	private static final String TAG = XWalkViewWithRedirection.class.getSimpleName();
-
     private XWalkView mXWalkView;
-    private TextView LoadstoppedTimes;
-    private int times_started = 0;
-    private int times_stopped = 0;
-    private int progress = 0;
+    private TextView state;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.version_layout);
+        setContentView(R.layout.activity_xwalk_view_with_redirection);
         mXWalkView = (XWalkView) findViewById(R.id.xwalkview);
     }
 
+    class UIResourceClient extends XWalkResourceClient{
 
-	class UIClient extends XWalkUIClient{
+        public UIResourceClient(XWalkView view) {
+            super(view);
+            // TODO Auto-generated constructor stub
+        }
 
-		public UIClient(XWalkView view) {
-			super(view);
-			// TODO Auto-generated constructor stub
-		}
+        @Override
+        public void onProgressChanged(XWalkView view, int progressInPercent) {
+            // TODO Auto-generated method stub
+            super.onProgressChanged(view, progressInPercent);
+            updateLoadingState();
+        }
+    }
 
-		@Override
-		public void onPageLoadStopped(XWalkView view, String url,
-				LoadStatus status) {
-			// TODO Auto-generated method stub
-			super.onPageLoadStopped(view, url, status);
-			Log.d(TAG, "onPageLoadStopped called");
-			times_stopped++;
-			updateLoadingState();
-		}
 
-		@Override
-		public void onPageLoadStarted(XWalkView view, String url) {
-			// TODO Auto-generated method stub
-			super.onPageLoadStarted(view, url);
-			Log.d(TAG, "onPageLoadStarted called");
-			times_started++;
-			updateLoadingState();
-		}
+    class UIClient extends XWalkUIClient{
 
-	}
+        public UIClient(XWalkView view) {
+            super(view);
+            // TODO Auto-generated constructor stub
+        }
 
-	class UIResourceClient extends XWalkResourceClient{
+        @Override
+        public void onPageLoadStopped(XWalkView view, String url,
+                LoadStatus status) {
+            // TODO Auto-generated method stub
+            super.onPageLoadStopped(view, url, status);
+            updateLoadingState();
+        }
 
-		public UIResourceClient(XWalkView view) {
-			super(view);
-			// TODO Auto-generated constructor stub
-		}
+        @Override
+        public void onPageLoadStarted(XWalkView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageLoadStarted(view, url);
+            updateLoadingState();
+        }
+    }
 
-		@Override
-		public void onProgressChanged(XWalkView view, int progressInPercent) {
-			// TODO Auto-generated method stub
-			super.onProgressChanged(view, progressInPercent);
-			Log.d(TAG, "Loading Progress:" + progressInPercent);
-			progress = progressInPercent;
-			updateLoadingState();
-		}
+    private void updateLoadingState() {
+        // TODO Auto-generated method stub
+        String s = "http://www.cnn.com/ ==> " + mXWalkView.getUrl();
+        state.setText(s);
+    }
 
-	}
-
-	private void updateLoadingState() {
-		// TODO Auto-generated method stub
-		String state = "onPageLoadStarted called: " + times_started +
-				       "\nonPageLoadStopped called: " + times_stopped +
-				       "\nonProgressChanged id: " + progress;
-		LoadstoppedTimes.setText(state);
-	}
-
-	@Override
-	protected void onXWalkReady() {
-		// TODO Auto-generated method stub
+    @Override
+    protected void onXWalkReady() {
         StringBuffer mess = new StringBuffer();
         mess.append("Test Purpose: \n\n")
-        .append("Load a page with redirection, track onPageLoadStopped, onPageLoadStarted, onProgressChanged called times.\n\n")
-        .append("Test  Step:\n\n")
-        .append("1. load the website http://www.cnn.com/ automatically.\n")
-        .append("2. observe the onProgressChanged id until it is 100.\n\n")
+        .append("Load a page with redirection, track onPageLoadStopped, onPageLoadStarted, onProgressChanged.\n\n")
         .append("Expected Result:\n\n")
-        .append("Test passes if onPageLoadStopped called shows 1 time");
+        .append("Test passes if the message show the redirecting website address");
         new  AlertDialog.Builder(this)
         .setTitle("Info" )
         .setMessage(mess.toString())
         .setPositiveButton("confirm" ,  null )
         .show();
 
-        LoadstoppedTimes = (TextView) findViewById(R.id.text1);
+        state = (TextView) findViewById(R.id.message_tv);
 
-        mXWalkView.setUIClient(new UIClient(mXWalkView));
         mXWalkView.setResourceClient(new UIResourceClient(mXWalkView));
+        mXWalkView.setUIClient(new UIClient(mXWalkView));
         mXWalkView.load("http://www.cnn.com/", null);
-	}
+    }
 }
