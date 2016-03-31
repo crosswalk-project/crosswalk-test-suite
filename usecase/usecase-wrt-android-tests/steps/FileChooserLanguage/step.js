6a6faa545f42a4d2ae6171cb25c5bc8fc654baab
Li Hao <haox.li@intel.com>
2016-03-31 14:18:35 +0800

    [usecase-wrt] Add File Chooser language test
    Failure reason:
    
    XWALK 5178 'File chooser' object does not assume device language settings and remains without translation.
    
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: Crosswalk Project for Android 19.48.503.0
    Unit test result summary: pass 0, fail 1, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/XWALK-5208

---
+++ usecase/usecase-wrt-android-tests/steps/FileChooserLanguage/step.js
@@ -0,0 +1,8 @@
+var step = '<font class="fontSize">'
+            +'<p>Purpose:</p>'
+            +'  <p>Verifies that the File Chooser label should assume the device language settings.</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'  <li>The label on the File Chooser button assume device language settings.</li>'
+            +'</ol>'
+          +'</font>';
