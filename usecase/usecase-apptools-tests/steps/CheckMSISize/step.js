3310c8a5f87a634c2f60f4390fb68f29f426890e
Liu Yun <yunx.liu@intel.com>
2016-04-06 14:00:14 +0800

    [usecase-apptools] Update tests to check apk size for usecase apptools windows
    Impacted tests(approved): new 0, update 1, delete 0
    Unit test platform: [Windows]
    Unit test result summary: pass 1, fail 0, block 0

---
+++ usecase/usecase-apptools-tests/steps/CheckMSISize/step.js
@@ -0,0 +1,6 @@
+var step = '<p>Purpose:</p>'
+            +'<p>Verifies if the size of basicapp and remotedebugging apk are nearly, and the size is less than 40M</p>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>The size of basicapp and remotedebugging apk are nearly, and the size is less than 40M</li>'
+            +'</ol>';
