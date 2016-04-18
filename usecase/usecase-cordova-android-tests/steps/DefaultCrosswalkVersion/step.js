c4e0ff4b36f22cb437fbabbe464e3682dcfb9c6f
Hao Li <haox.li@intel.com>
2016-04-18 13:33:31 +0800

    [usecase-cordova] Add shared mode support and copyright (#3546)
    Update sub cases which not support to pack embedded/shared
    Add copyright for python script
    Impacted tests(approved): new 0, update 1, delete 0
    Unit test platform: Crosswalk Project for Android 19.49.509.0
    Unit test result summary: pass 1, fail 0, block 0

--- usecase/usecase-cordova-android-tests/steps/DefaultCrosswalkVersion/step.js
+++ usecase/usecase-cordova-android-tests/steps/DefaultCrosswalkVersion/step.js
@@ -2,5 +2,5 @@ var step = '<font class="fontSize">'
             +'<p>Purpose:</p>'
             +'<p>Verify the webview plugin support default crosswalk version</p>'
             +'<p>Expected Result:</p>'
-            +'<li>The crosswalk version is the same with the latest version in https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library/</li>'
+            +'<li>The crosswalk version is the same with the latest version in https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library_beta/(shared mode should be https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_shared_library_beta/)</li>'
           +'</font>';
