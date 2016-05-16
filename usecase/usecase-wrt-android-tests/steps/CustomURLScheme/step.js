7609768ba98c9b4866c5b9a9af2a03451eef7569
Hao Li <hao.x.li@intel.com>
2016-05-16 10:05:20 +0800

    [usecase-wrt] Add test for Custom URL Scheme (#3603)
    Impacted tests(approved): new 1, update 0, delete 0
    Unit test platform: Crosswalk for Android 20.49.527.0
    Unit test result summary: pass 1, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/CTS-609

---
+++ usecase/usecase-wrt-android-tests/steps/CustomURLScheme/step.js
@@ -0,0 +1,13 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Steps</p>'
+            +'<ol>'
+            +'<li>Click "Hello, World!".</li>'
+            +'<li>Click "WeChat".</li>'
+            +'</ol>'
+            +'<p>Expected Result</p>'
+            +'<ol>'
+            +'<li>If WhatsApp was installed, it will be launched, and can send "Hello, World!" to your contacts if you have. If it was not installed, will report "ERR_UNKNOWN_URL_SCHEME".</li>'
+            +'<li>If WeChat(WeiXin) was installed, it will be launched. If it was not installed, XWalkView will load its download page.</li>'
+            +'</ol>'
+          +'</font>';
+
