69571f8e737f7c20f40568520601f069e4c5c17b
qiuzhong <zhongx.qiu@intel.com>
2016-05-30 10:10:28 +0800

    [release-script]Update build_ios.py for pack.py iOS backend
    Update the build_ios.py to build ipa files for iOS deivces via
    crosswalk-app as crosswalk-pkg for iOS is not supported.
    
    Verified the ipa files can be built successfully.
    
    BUG=https://crosswalk-project.org/jira/browse/CTS-612

--- tools/build/build_ios.py
+++ tools/build/build_ios.py
@@ -28,8 +28,61 @@
 # Authors:
 #         Zhong, Qiu <zhongx.qiu@intel.com>
 
+import os
+import glob
+import utils
+import varshop
+
+DEFAULT_CMD_TIMEOUT = 600
+
 
 def packIOS(build_json = None, app_src = None, app_dest = None, app_name = None):
-    # For iOS platform, ignore the building ipa files at present.
-    # Still keep this function if there will be any changes in the future.
+    '''
+    Build iOS ipa via crosswalk-app, currently crosswalk-pkg for iOS does not
+    support yet.
+    '''
+    BUILD_ROOT = varshop.getValue("BUILD_ROOT")
+
+    package_id = 'org.xwalk.{app_name}'.format(
+                            app_name = app_name.replace('-', ''))
+    create_cmd = 'crosswalk-app create {package_id} --platform=ios'.format(
+                                        package_id = package_id)
+
+    orig_dir = os.getcwd()
+    os.chdir(BUILD_ROOT)
+    if not utils.doCMD(create_cmd, DEFAULT_CMD_TIMEOUT):
+        os.chdir(orig_dir)
+        return False
+
+    move_app_cmd =  'mv -fv {app_src}/* ' \
+                    '{BUILD_ROOT}/{package_id}/app/'.format(
+                        app_src = app_src,
+                        BUILD_ROOT = BUILD_ROOT,
+                        package_id = package_id)
+    if not utils.doCMD(move_app_cmd, DEFAULT_CMD_TIMEOUT):
+        os.chdir(orig_dir)
+        return False
+
+    os.chdir(package_id)
+    create_cmd = 'crosswalk-app build'
+    if not utils.doCMD(create_cmd, DEFAULT_CMD_TIMEOUT):
+        os.chdir(orig_dir)
+        return False
+
+    ipa_files = glob.glob(os.path.join(BUILD_ROOT, package_id, "*.ipa"))
+    if ipa_files:
+        rename_app_name = app_name.replace('-', '_')
+        if not utils.doCopy(ipa_files[0],
+                            os.path.join(
+                                app_dest,
+                                "{app_name}.ipa".format(
+                                    app_name = rename_app_name)
+                            )):
+            os.chdir(orig_dir)
+            return False
+    else:
+        LOG.error("Fail to find the ipa file")
+        os.chdir(orig_dir)
+        return False
+
     return True
