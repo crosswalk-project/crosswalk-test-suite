191dfc1b81528ab858839036bcf7b66272dae716
zhuyongyong <yongyongx.zhu@intel.com>
2016-06-14 09:57:15 +0800

    [Embeddingapi] Update ClearSslPreferenceTest usecase (#3645)
    Impacted tests(approved): new 0, update 2, delete 0
    Unit test platform: Crosswalk Project for Android 21.50.537.0
    Unit test result summary: pass 2, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/CTS-1713

--- embeddingapi/embedding-api-android-tests/embeddingapi/src/org/xwalk/embedding/base/XWalkViewTestBase.java
+++ embeddingapi/embedding-api-android-tests/embeddingapi/src/org/xwalk/embedding/base/XWalkViewTestBase.java
@@ -656,8 +656,10 @@ public class XWalkViewTestBase extends ActivityInstrumentationTestCase2<MainActi
         @Override
         public void onReceivedSslError(XWalkView view,
                 ValueCallback<Boolean> callback, SslError error) {
-            callback.onReceiveValue(mAllowSslError);
-            mTestHelperBridge.onReceivedSsl();
+            if(error.getUrl().endsWith("html")){
+                callback.onReceiveValue(mAllowSslError);
+                mTestHelperBridge.onReceivedSsl();
+            }
         }
     }
 
