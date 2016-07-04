0ac395ba8a0aadd53733614f57416eb025de73b8
Li Hao <hao.x.li@intel.com>
2016-07-04 15:06:32 +0800

    [Crosswalk-20][usecase-cordova] Update DefaultCrosswalkVersion to latest stable version
    Impacted tests(approved): new 0, update 1, delete 0
    Unit test platform: Crosswalk for Android 19.49.514.4
    Unit test result summary: pass 1, fail 0, block 0
    
    https://crosswalk-project.org/jira/browse/CTS-1798

--- usecase/usecase-cordova-android-tests/steps/DefaultCrosswalkVersion/step.js
+++ usecase/usecase-cordova-android-tests/steps/DefaultCrosswalkVersion/step.js
@@ -2,5 +2,5 @@ var step = '<font class="fontSize">'
             +'<p>Purpose:</p>'
             +'<p>Verify the webview plugin should default to crosswalk latest stable version</p>'
             +'<p>Expected Result:</p>'
-            +'<li>The crosswalk version is the same with the latest version in https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library_beta/</li>'
+            +'<li>The crosswalk version is the same with the latest version in https://download.01.org/crosswalk/releases/crosswalk/android/maven2/org/xwalk/xwalk_core_library/</li>'
           +'</font>';
