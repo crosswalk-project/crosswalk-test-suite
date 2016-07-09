f51ec12dfd696eb7c4aab52d7f2844386de8c4b0
Li, Hao <hao.x.li@intel.com>
2016-07-09 15:59:30 +0800

    [Cordova] Update Cordova package mode testing
    This case still Cordova 3.6.x build way, update to latest and add shared mode testing.
    
    Impacted tests(approved): new 0, update 1, delete 0
    Unit test platform: Crosswalk for Android 21.51.545.0
    Unit test result summary: pass 1, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/CTS-1765

--- usecase/usecase-cordova-android-tests/samples/CordovaPackage/res/test.py
+++ usecase/usecase-cordova-android-tests/samples/CordovaPackage/res/test.py
@@ -27,6 +27,7 @@
 #
 # Authors:
 #         Zhu, Yongyong <yongyongx.zhu@intel.com>
+#         Li, Hao <haox.li@intel.com>
 
 import os
 import commands
@@ -35,17 +36,26 @@ import json
 sys.path.append(os.getcwd())
 sys.path.append(os.path.realpath('..'))
 import comm
+import xml.dom.minidom
 from optparse import OptionParser
+
+
 PKG_MODES = ["shared", "embedded"]
+PKG_ARCH = ["x86", "x86_64", "arm", "arm64"]
 
 comm.setUp()
 try:
-    usage = "Usage: ./test.py -m shared"
+    usage = "Usage: ./test.py -m [embedded|shared] -a [x86|x86_64|arm|arm64]"
     opts_parser = OptionParser(usage=usage)
     opts_parser.add_option(
         "-m",
         "--mode",
         dest="pkgmode",
+        help="specify the apk mode, e.g. shared, embedded")
+    opts_parser.add_option(
+        "-a",
+        "--arch",
+        dest="pkgarch",
         help="specify the apk mode, e.g. shared, embedded")
     global BUILD_PARAMETERS
     (BUILD_PARAMETERS, args) = opts_parser.parse_args()
@@ -56,26 +66,25 @@ if not BUILD_PARAMETERS.pkgmode:
     print "Please add the -m parameter for the pkgmode"
     sys.exit(1)
 elif BUILD_PARAMETERS.pkgmode and not BUILD_PARAMETERS.pkgmode in PKG_MODES:
-    print "Wrong pkg-mode, only support: %s, exit ..." % PKG_MODES
+    print "Wrong pkg mode, only support: %s, exit ..." % PKG_MODES
+    sys.exit(1)
+if BUILD_PARAMETERS.pkgarch and not BUILD_PARAMETERS.pkgarch in PKG_ARCH:
+    print "Wrong pkg arch, only support: %s, exit ..." % PKG_ARCH
     sys.exit(1)
 
-comm.installCrosswalk(BUILD_PARAMETERS.pkgmode)
+comm.installCrosswalk(BUILD_PARAMETERS.pkgmode, BUILD_PARAMETERS.pkgarch)
 app_name = "CordovaPackage"
 pkg_name = "com.example.cordovaPackage2"
 
 current_path_tmp = os.getcwd()
-cordova_android_path = os.path.join(current_path_tmp, "cordova-android")
-if os.path.exists(cordova_android_path):
-    comm.doRemove([cordova_android_path])
-os.system("git clone https://github.com/apache/cordova-android.git")
-
 project_path = os.path.join(current_path_tmp, app_name)
 if os.path.exists(project_path):
     comm.doRemove([project_path])
 
-os.system("cordova-android/bin/create %s %s %s" % (app_name, pkg_name, app_name))
+os.system("cordova create %s %s %s" % (app_name, pkg_name, app_name))
 os.chdir(project_path)
-os.system("plugman install --platform android --plugin ../../../tools/cordova-plugin-crosswalk-webview/ --project .")
+os.system("cordova platform add android")
+
 
 pkg_mode_tmp = "shared"
 if BUILD_PARAMETERS.pkgmode == "embedded":
@@ -85,14 +94,43 @@ xwalk_version = "%s" % comm.CROSSWALK_VERSION
 if comm.CROSSWALK_BRANCH == "beta":
     xwalk_version = "org.xwalk:xwalk_%s_library_beta:%s" % (pkg_mode_tmp, comm.CROSSWALK_VERSION)
 
-os.system('sed -i "s/<preference default=\\".*\\" name=\\"xwalkVersion\\"/<preference default=\\"%s\\" name=\\"xwalkVersion\\"/g" res/xml/config.xml' % xwalk_version)
 
-if BUILD_PARAMETERS.pkgmode == "shared":
-    os.system('sed -i "s/<preference default=\\"embedded\\" name=\\"xwalkMode\\"/<preference default=\\"shared\\" name=\\"xwalkMode\\"/g" res/xml/config.xml')
- 
-os.system("./cordova/build")
-os.system("./cordova/run")
+os.system("cordova plugin add %s --variable XWALK_VERSION='%s' --variable XWALK_MODE='%s'" % (comm.plugin_tool, xwalk_version, BUILD_PARAMETERS.pkgmode))
 
-comm.checkApkExist("./build/outputs/apk/*.apk")
+# open config.xml
+dom = xml.dom.minidom.parse("config.xml")
+root = dom.documentElement
+preferences = root.getElementsByTagName('preference')
+# check the xwalk version set correctly by "--variable XWALK_VERSION"
+# check the xwalk mode set correctly by "--variable XWALK_MODE"
+for node in preferences:
+    if node.nodeName == "xwalkVersion":
+        if node.nodeValue != xwalk_version:
+            print "Incorrect xwalk version in config.xml: %s, exit ..." % node.nodeValue
+            sys.exit(1)
+    if node.nodeName == "xwalkMode":
+        if node.nodeValue != BUILD_PARAMETERS.pkgmode:
+            print "Incorrect xwalk mode in config.xml: %s, exit ..." % node.nodeValue
+            sys.exit(1)
+
+
+pack_arch_opt = ""
+if BUILD_PARAMETERS.pkgarch and BUILD_PARAMETERS.pkgmode == "embedded":
+    pack_arch_type = BUILD_PARAMETERS.pkgarch
+    if BUILD_PARAMETERS.pkgarch == "x86":
+        pack_arch_type = "x86"
+    elif BUILD_PARAMETERS.pkgarch == "x86_64":
+        pack_arch_type = "x86 --xwalk64bit"
+    elif BUILD_PARAMETERS.pkgarch == "arm":
+        pack_arch_type = "armv7"
+    elif BUILD_PARAMETERS.pkgarch == "arm64":
+        pack_arch_type = "armv7 --xwalk64bit"
+    pack_arch_opt = "-- --gradleArg=-PcdvBuildArch=%s" % pack_arch_type
+
+
+os.system("cordova build android %s" % pack_arch_opt)
+os.system("cordova run android %s" % pack_arch_opt)
+
+comm.checkApkExist("./platforms/android/build/outputs/apk/*.apk")
 comm.checkApkRun(pkg_name)
 
