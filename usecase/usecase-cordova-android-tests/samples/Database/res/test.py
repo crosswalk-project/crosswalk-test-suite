2422af0ea7dde4944915ae20dcc83a01cd6c33aa
qiuzhong <zhongx.qiu@intel.com>
2016-07-18 16:15:34 +0800

    [Cordova] Upgrade the value of minSdkVersion parameter from 14(default) to 16 when building cordova tests. (#3702) (#3706)
    * [Cordova] Upgrade the value of minSdkVersion parameter from 14(default) to 16 when building cordova tests.
    BUG=https://crosswalk-project.org/jira/browse/CTS-1831
    
    * [Cordova] Upgrade the value of minSdkVersion parameter from 14(default) to 16 when running cordova tests.

--- usecase/usecase-cordova-android-tests/samples/Database/res/test.py
+++ usecase/usecase-cordova-android-tests/samples/Database/res/test.py
@@ -36,7 +36,7 @@ os.system("cordova create database org.xwalk.database database")
 os.chdir("./database")
 os.system("cordova platform add android")
 os.system("cp -r ../database_source/* www/")
-os.system("cordova build android")
+os.system("cordova build android -- --minSdkVersion=16")
 lsstatus = commands.getstatusoutput("ls ./platforms/android/build/outputs/apk/android-debug.apk &>/dev/null")
 if lsstatus[0] == 0:
     os.system("cp ./platforms/android/build/outputs/apk/android-debug.apk ../../../database_upstream.apk")
