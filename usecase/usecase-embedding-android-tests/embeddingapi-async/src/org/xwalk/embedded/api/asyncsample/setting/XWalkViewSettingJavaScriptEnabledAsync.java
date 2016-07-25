d5ee45558cd7af8e8efd9faa550fdec9cb62801c
Li, Hao <hao.x.li@intel.com>
2016-07-25 10:29:21 +0800

    [Crosswalk-21][usecase-embeddingapi]Add tests for setJavaScript related APIs
    Impacted tests(approved): new 4, update 0, delete 0
    Unit test platform: Crosswalk for Android 21.51.546.0
    Unit test result summary: pass 4, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/CTS-1818

--- usecase/usecase-embedding-android-tests/embeddingapi-async/src/org/xwalk/embedded/api/asyncsample/setting/XWalkViewSettingJavaScriptEnabledAsync.java
+++ usecase/usecase-embedding-android-tests/embeddingapi-async/src/org/xwalk/embedded/api/asyncsample/setting/XWalkViewSettingJavaScriptEnabledAsync.java
@@ -1,6 +1,7 @@
 package org.xwalk.embedded.api.asyncsample.setting;
 
 import org.xwalk.core.XWalkInitializer;
+import org.xwalk.core.XWalkSettings;
 import org.xwalk.core.XWalkView;
 import org.xwalk.embedded.api.asyncsample.R;
 
@@ -11,17 +12,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class XWalkViewSettingDomStorageEnabledAsync extends Activity implements XWalkInitializer.XWalkInitListener {
+public class XWalkViewSettingJavaScriptEnabledAsync extends Activity implements XWalkInitializer.XWalkInitListener {
 
     private XWalkInitializer mXWalkInitializer;
 
-    public final static String ENABLE_BT = "Enable DOM Storage";
-    public final static String DISABLE_BT = "Disable DOM Storage";
-    public final static String MESSAGE_TITLE = "DOM Storage Enabled: ";
+    public final static String ENABLE_BT = "Enable JavaScript";
+    public final static String DISABLE_BT = "Disable JavaScript";
+    public final static String MESSAGE_TITLE = "JavaScript Enabled: ";
 
     private XWalkView mXWalkView;
     private TextView mMessage;
     private Button mButton;
+    private XWalkSettings settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,13 +50,13 @@ public class XWalkViewSettingDomStorageEnabledAsync extends Activity implements 
 
     @Override
     public void onXWalkInitCompleted() {
-        setContentView(R.layout.activity_xwalk_view_setting_dom_storage_enable_async);
+        setContentView(R.layout.activity_xwalk_view_setting_javascript_enabled_async);
 
         StringBuffer mess = new StringBuffer();
         mess.append("Test Purpose: \n\n")
-                .append("Check if XWalkView can enable/disable DOM storage API.\n\n")
+                .append("Check that XWalkView can enable/disable JavaScript execution.\n\n")
                 .append("Expected Result:\n\n")
-                .append("Test passes if Dom storage can be enabled/disabled");
+                .append("Test passes if JavaScript can be enabled/disabled");
         new  AlertDialog.Builder(this)
                 .setTitle("Info")
                 .setMessage(mess.toString())
@@ -64,27 +66,28 @@ public class XWalkViewSettingDomStorageEnabledAsync extends Activity implements 
         mXWalkView = (XWalkView) findViewById(R.id.xwalk_view);
         mMessage = (TextView) findViewById(R.id.message_tv);
         mButton = (Button) findViewById(R.id.switch_al);
-        boolean defaultStorage = mXWalkView.getSettings().getDomStorageEnabled();
-        if (defaultStorage) {
+        settings = mXWalkView.getSettings();
+        boolean defaultValue = settings.getJavaScriptEnabled();
+        if (defaultValue) {
             mButton.setText(DISABLE_BT);
         }else{
             mButton.setText(ENABLE_BT);
         }
-        mMessage.setText(MESSAGE_TITLE + defaultStorage + "(Default)");
-        mXWalkView.load("file:///android_asset/dom_storage_enable.html", null);
+        mMessage.setText(MESSAGE_TITLE + defaultValue + "(Default)");
+        mXWalkView.load("file:///android_asset/javascript_enabled.html", null);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (ENABLE_BT.equals(mButton.getText())) {
-                    mXWalkView.getSettings().setDomStorageEnabled(true);
+                    settings.setJavaScriptEnabled(true);
                     mButton.setText(DISABLE_BT);
                 } else {
-                    mXWalkView.getSettings().setDomStorageEnabled(false);
+                    settings.setJavaScriptEnabled(false);
                     mButton.setText(ENABLE_BT);
                 }
-                mMessage.setText(MESSAGE_TITLE + mXWalkView.getSettings().getDomStorageEnabled());
-                mXWalkView.load("file:///android_asset/dom_storage_enable.html", null);
+                mMessage.setText(MESSAGE_TITLE + settings.getJavaScriptEnabled());
+                mXWalkView.load("file:///android_asset/javascript_enabled.html", null);
 
             }
         });
