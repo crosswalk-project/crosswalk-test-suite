7acd07d129a47051aae1735a2fcd01ee93115648
Hao Li <hao.x.li@intel.com>
2016-07-26 14:31:08 +0800

    [usecase-embeddingapi]Add test for set/getSaveFormData methods (#3728)
    Impacted tests(approved): new 2, update 0, delete 0
    Unit test platform: Crosswalk for Android 21.51.546.0
    Unit test result summary: pass 2, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/CTS-1821

---
+++ usecase/usecase-embedding-android-tests/embeddingapi/src/org/xwalk/embedded/api/sample/setting/XWalkViewSettingSaveFormData.java
@@ -0,0 +1,79 @@
+package org.xwalk.embedded.api.sample.setting;
+
+import org.xwalk.core.XWalkActivity;
+import org.xwalk.core.XWalkSettings;
+import org.xwalk.core.XWalkView;
+import org.xwalk.embedded.api.sample.R;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class XWalkViewSettingSaveFormData extends XWalkActivity {
+
+    public final static String ENABLE_BT = "Set SaveFormData(ture)";
+    public final static String DISABLE_BT = "Set SaveFormData(false)";
+    public final static String MESSAGE_TITLE = "Get SaveFormData: ";
+
+    private XWalkView mXWalkView;
+    private TextView mMessage;
+    private Button mButton;
+    private XWalkSettings settings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xwalk_view_setting_save_form_data);
+    }
+
+    @Override
+    protected void onXWalkReady() {
+        StringBuffer mess = new StringBuffer();
+        mess.append("Test Purpose: \n\n")
+                .append("Check if XWalkView can set/get SaveFormData in XWalkSettings.\n\n")
+                .append("Test Steps:\n\n")
+                .append("1. Set SaveFormData to true if it's false.\n\n")
+                .append("2. Input your name.\n\n")
+                .append("3. Click submit button.\n\n")
+                .append("4. Input first letter of your name or double click input area.\n\n")
+                .append("5. Set SaveFormData to false.\n\n")
+                .append("6. Input first letter of your name again or double click input area.\n\n")
+                .append("Expected Result:\n\n")
+                .append("Test passes if the saved form date does not auto complete(or popup) after set SaveFormData to false.");
+        new  AlertDialog.Builder(this)
+                .setTitle("Info")
+                .setMessage(mess.toString())
+                .setPositiveButton("confirm", null)
+                .show();
+
+        mXWalkView = (XWalkView) findViewById(R.id.xwalk_view);
+        mMessage = (TextView) findViewById(R.id.message_tv);
+        mButton = (Button) findViewById(R.id.switch_al);
+        settings = mXWalkView.getSettings();
+        boolean defaultvalue = settings.getSaveFormData();
+        if (defaultvalue) {
+            mButton.setText(DISABLE_BT);
+        }else{
+            mButton.setText(ENABLE_BT);
+        }
+        mMessage.setText(MESSAGE_TITLE + defaultvalue + "(Default)");
+        mXWalkView.load("file:///android_asset/save_form_data.html", null);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ENABLE_BT.equals(mButton.getText())) {
+                    settings.setSaveFormData(true);
+                    mButton.setText(DISABLE_BT);
+                } else {
+                    settings.setSaveFormData(false);
+                    mButton.setText(ENABLE_BT);
+                }
+                mMessage.setText(MESSAGE_TITLE + settings.getSaveFormData());
+                mXWalkView.load("file:///android_asset/save_form_data.html", null);
+            }
+        });
+    }
+}
