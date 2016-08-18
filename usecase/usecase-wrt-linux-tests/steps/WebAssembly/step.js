07b82f107c9bb453f6cae41653b2eae449ee18f2
JiangXiuqi <xiuqix.jiang@intel.com>
2016-08-18 13:18:03 +0800

    Add the usecase of the  WebAssembly.

---
+++ usecase/usecase-wrt-linux-tests/steps/WebAssembly/step.js
@@ -0,0 +1,6 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Purpose: </p>'
+            +'<p>Verify if the demo can work.</p>'
+            +'<p>Expected Result: </p>'
+            +'<p>Test passes if the demo work well.</p>'
+          +'</font>';
