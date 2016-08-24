21d3e15352ff1f36736cc1ef0c3def12dcb4a31b
Li, Hao <hao.x.li@intel.com>
2016-08-24 20:51:28 +0800

    [Embeddingapi] Add tests for new exposed APIs
    Test APIs:
    setLoadsImagesAutomatically/getLoadsImagesAutomatically
    set/getBlockNetworkImage
    set/getMediaPlaybackRequiresUserGesture
    set/getSupportMultipleWindows
    
    Impacted tests(approved): new 10, update 0, delete 0
    Unit test platform: Crosswalk for Android 22.52.561.0
    Unit test result summary: pass 10, fail 0, block 0
    
    BUG=https://crosswalk-project.org/jira/browse/CTS-1841

--- embeddingapi/embedding-asyncapi-android-tests/embeddingapi/src/org/xwalk/embedding/base/OnCreateWindowRequestedHelper.java
+++ embeddingapi/embedding-asyncapi-android-tests/embeddingapi/src/org/xwalk/embedding/base/OnCreateWindowRequestedHelper.java
@@ -1,9 +1,11 @@
 package org.xwalk.embedding.base;
 
 import org.chromium.content.browser.test.util.CallbackHelper;
+import org.xwalk.core.XWalkView;
 
 public class OnCreateWindowRequestedHelper extends CallbackHelper {
     private boolean mCalled = false;
+    private XWalkView mXWalkView;
 
     public boolean getCalled() {
         assert getCallCount() > 0;
@@ -12,6 +14,16 @@ public class OnCreateWindowRequestedHelper extends CallbackHelper {
 
     public void notifyCalled(boolean called) {
         mCalled = called;
+        notifyCalled();
+    }
+    
+    public XWalkView getXWalkView() {
+        assert getCallCount() > 0;
+        return mXWalkView;
+    }
+
+    public void notifyCalled(XWalkView view) {
+        mXWalkView = view;
         notifyCalled();
     }
 }
