f9e65945e28d39f1d4eafd3c18372ad1416e6eab
kaixinjxq <jxq-86@163.com>
2016-09-14 14:18:58 +0800

    Add Viewport in usecase. (#3779)

---
+++ usecase/usecase-webapi-xwalk-tests/steps/Viewport/step.js
@@ -0,0 +1,6 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Purpose: </p>'
+            +'<p>Verify if the viewport functionality of setting customized height and width, device height and width, scale, enabling or disabling user scalable to allow or disallow zoom in/out screen, sliding the screen by figure work well.</p>'
+            +'<p>Expected Result: </p>'
+            +'<p>Test passes if the viewport functionality of setting customized height and width, device height and width, scale, enabling or disabling user scalable to allow or disallow zoom in/out screen, sliding the screen by figure work well.</p>'
+          +'</font>';
