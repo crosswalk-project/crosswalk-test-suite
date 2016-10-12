68150f6814ccb7e3a908681cbd34d39e3727039e
JiangXiuqi <xiuqix.jiang@intel.com>
2016-10-12 10:33:03 +0800

    Add the input color checkpoint.

---
+++ usecase/usecase-wrt-android-tests/steps/InputColor/step.js
@@ -0,0 +1,12 @@
+var step = '<font class="fontSize">'
+            +'<p>Test Steps:</p>'
+            +'<ol>'
+            +'<li>Click the color picker button</li>'
+            +'<li>Select the different color in the color picker window</li>'
+            +'</ol>'
+            +'<p>Expected Result:</p>'
+            +'<ol>'
+            +'<li>Display a color picker window</li>'
+            +'<li>The color of the color picker button is changed successfully</li>'
+            +'</ol>'
+          +'</font>';
