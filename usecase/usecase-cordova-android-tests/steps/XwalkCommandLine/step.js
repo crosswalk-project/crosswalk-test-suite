f8f143f05f4cb27c58be8c5f8087451ac0178d7e
Li, Hao <hao.x.li@intel.com>
2016-10-26 20:04:19 +0800

    [Crosswalk-22][usecase-cordova]Remove --disable-webrtc test
    Impacted tests(approved): new 0, update 1, delete 0
    Unit test platform: Crosswalk Project for Android 22.52.561.0
    Unit test result summary: pass 1, fail 0, block 0
    
    https://crosswalk-project.org/jira/browse/CTS-1908

--- usecase/usecase-cordova-android-tests/steps/XwalkCommandLine/step.js
+++ usecase/usecase-cordova-android-tests/steps/XwalkCommandLine/step.js
@@ -6,6 +6,6 @@ var step = '<font class="fontSize">'
             +'<li>The app can be installed successfully</li>'
             +'<li>The app can launch and run successfully</li>'
             +'<li>The app page does not refresh and not cause ugly behaviour</li>'
-            +'<li>There are green "Pass" below "Disable WebGL" and "Disable WebRTC" on the app page</li>'
+            +'<li>There are green "Pass" below "Disable WebGL" on the app page</li>'
             +'</ol>'
           +'</font>';
